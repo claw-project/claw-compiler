04a1e73dc4b496437d41b448b9a4a6e585f0f98b
Valentin Clement <clementval@gmail.com>
2015-11-02 12:29:02 +0100

    Mix loop-fusion with and without group option

--- test/loops/fusion2/original_code.f90
+++ test/loops/fusion2/original_code.f90
@@ -7,22 +7,32 @@ END
 SUBROUTINE clawloop
   INTEGER :: i
   !$claw loop-fusion group(g1)
-  DO i=1,10
+  DO i=1,5
     PRINT *, 'First loop body:',i
   END DO
 
   !$claw loop-fusion group(g1)
-  DO i=1,10
+  DO i=1,5
     PRINT *, 'Second loop body:',i
   END DO
 
   !$claw loop-fusion group(g2)
-  DO i=1,10
+  DO i=1,5
     PRINT *, 'Third loop body:',i
   END DO
 
   !$claw loop-fusion group(g2)
-  DO i=1,10
+  DO i=1,5
     PRINT *, 'Fourth loop body:',i
+  END DO
+
+  !$claw loop-fusion
+  DO i=1,5
+    PRINT *, 'Fifth loop body:',i
+  END DO
+
+  !$claw loop-fusion
+  DO i=1,5
+    PRINT *, 'Sixth loop body:',i
   END DO
 END
