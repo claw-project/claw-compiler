753d90a17a3b1817cc05e84015491706991a0bca
Valentin Clement <clementval@gmail.com>
2015-11-03 11:31:42 +0100

    Add 2 additional groups of loops to the test

--- test/loops/interchange2/original_code.f90
+++ test/loops/interchange2/original_code.f90
@@ -5,11 +5,29 @@ PROGRAM LOOP_INTERCHANGE
 END
 
 SUBROUTINE clawloop
-  INTEGER :: i, j, k
+  INTEGER :: i, j, k, a, b, c, x, y, z
   !$claw loop-interchange new-order(k,i,j)
   DO i=1,4
     DO j=1,3
       DO k=1,2
+        PRINT *, 'Iteration i=',i,', j=',j,', k=',k
+      END DO
+    END DO
+  END DO
+
+  !$claw loop-interchange new-order(b,c,a)
+  DO a=1,4
+    DO b=1,3
+      DO c=1,2
+        PRINT *, 'Iteration i=',i,', j=',j,', k=',k
+      END DO
+    END DO
+  END DO
+
+  !$claw loop-interchange new-order(x,z,y)
+  DO x=1,4
+    DO y=1,3
+      DO z=1,2
         PRINT *, 'Iteration i=',i,', j=',j,', k=',k
       END DO
     END DO
