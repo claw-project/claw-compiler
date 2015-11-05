2c639dae01b813dc6a766731ba7dd31c0672f27a
Valentin Clement <clementval@gmail.com>
2015-11-05 11:54:20 +0100

    Add test code for the to-loop directive

---
+++ test/loops/vector1/original_code.f90
@@ -0,0 +1,21 @@
+! Simple program to test the to-loop directive
+
+PROGRAM VECTOR_LOOP
+  CALL claw
+END
+
+! Before the transformation
+SUBROUTINE claw
+  INTEGER :: j
+  INTEGER :: i = 10
+  INTEGER, DIMENSION(0:9) :: vec1
+
+  DO j = 0, i
+  		vec1(j) = j
+  END DO
+
+  !$claw to-loop
+  vec1(0:i) = vec1(0:i) + 10;
+
+  PRINT*,vec1
+END SUBROUTINE claw
---
+++ test/loops/vector1/reference.f90
@@ -0,0 +1,18 @@
+PROGRAM vector_loop
+
+ CALL claw ( )
+END PROGRAM vector_loop
+
+SUBROUTINE claw ( )
+ INTEGER :: j
+ INTEGER :: i = 10
+ INTEGER :: vec1 ( 0 : 9 )
+
+ DO j = 0 , i , 1
+  vec1 ( j ) = j
+ END DO
+!$claw to-loop
+ vec1 ( 0 : i ) = vec1 ( 0 : i ) + 10
+ PRINT * , vec1
+END SUBROUTINE claw
+
