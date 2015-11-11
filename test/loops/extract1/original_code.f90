f18b6848efc2286a03e7419e64a4565a3190b5d7
Valentin Clement <clementval@gmail.com>
2015-11-11 07:35:59 +0100

    Add test code for loop-extract

---
+++ test/loops/extract1/original_code.f90
@@ -0,0 +1,24 @@
+! Simple program to test the loop-extract directive
+
+PROGRAM LOOP_EXTRACT
+  INTEGER :: value1(10)
+  INTEGER :: value2(10)
+
+
+  !$claw loop-extract range(i=istart,iend) map(value1,value2:i)
+  CALL clawloop(value1, value2)
+END
+
+SUBROUTINE clawloop(value1, value2)
+  INTEGER :: value1(10)
+  INTEGER :: value2(10)
+  INTEGER :: i
+  INTEGER :: istart=1
+  INTEGER :: iend=10
+
+  DO i=istart,iend
+    print*,'value1: ',value1(i)
+    print*,'value2: ',value2(i)
+  END DO
+
+END
