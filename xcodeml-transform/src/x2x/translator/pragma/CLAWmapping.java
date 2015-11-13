27142d47bdf78c9af8bb4686a4009e2a50cdb906
Valentin Clement <clementval@gmail.com>
2015-11-13 15:57:58 +0100

    Add class skeleton for mapping in loop-extract

---
+++ xcodeml-transform/src/x2x/translator/pragma/CLAWmapping.java
@@ -0,0 +1,20 @@
+package x2x.translator.pragma;
+
+import java.util.ArrayList;
+
+public class CLAWmapping {
+
+  private int _mappedDimension = 0;
+  private ArrayList<String> _mappedVariables = null;
+  private ArrayList<String> _mappingVariables = null;
+
+
+  public CLAWmapping(String pragmaClause){
+
+  }
+
+  public int getMappedDimension(){
+    return _mappedDimension;
+  }
+
+}
