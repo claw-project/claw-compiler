240607ee383679af5274fb2e46f42c9d4b561678
Valentin Clement <clementval@gmail.com>
2015-11-19 10:10:31 +0100

    Move utility methods to XelementHelper

--- xcodeml-transform/src/x2x/translator/xcodeml/xelement/XelementHelper.java
+++ xcodeml-transform/src/x2x/translator/xcodeml/xelement/XelementHelper.java
@@ -150,4 +150,62 @@ public class XelementHelper {
     return null;
   }
 
+  /**
+   * Removes text nodes that only contains whitespace. The conditions for
+   * removing text nodes, besides only containing whitespace, are: If the
+   * parent node has at least one child of any of the following types, all
+   * whitespace-only text-node children will be removed: - ELEMENT child -
+   * CDATA child - COMMENT child
+   *
+   * The purpose of this is to make the format() method (that use a
+   * Transformer for formatting) more consistent regarding indenting and line
+   * breaks.
+   */
+  public static void cleanEmptyTextNodes(Node parentNode) {
+    boolean removeEmptyTextNodes = false;
+    Node childNode = parentNode.getFirstChild();
+    while (childNode != null) {
+      removeEmptyTextNodes |= checkNodeTypes(childNode);
+      childNode = childNode.getNextSibling();
+    }
+
+    if (removeEmptyTextNodes) {
+      removeEmptyTextNodes(parentNode);
+    }
+  }
+
+  private static void removeEmptyTextNodes(Node parentNode) {
+    Node childNode = parentNode.getFirstChild();
+    while (childNode != null) {
+      // grab the "nextSibling" before the child node is removed
+      Node nextChild = childNode.getNextSibling();
+
+      short nodeType = childNode.getNodeType();
+      if (nodeType == Node.TEXT_NODE) {
+        boolean containsOnlyWhitespace = childNode.getNodeValue()
+          .trim().isEmpty();
+        if (containsOnlyWhitespace) {
+          parentNode.removeChild(childNode);
+        }
+      }
+      childNode = nextChild;
+    }
+  }
+
+  private static boolean checkNodeTypes(Node childNode) {
+    short nodeType = childNode.getNodeType();
+
+    if (nodeType == Node.ELEMENT_NODE) {
+      cleanEmptyTextNodes(childNode); // recurse into subtree
+    }
+
+    if (nodeType == Node.ELEMENT_NODE
+        || nodeType == Node.CDATA_SECTION_NODE
+        || nodeType == Node.COMMENT_NODE) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
