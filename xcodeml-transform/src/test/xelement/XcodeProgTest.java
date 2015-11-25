e2e530ceaf3e77d6a3332a4e97c6d2c6ded43d32
Valentin Clement <clementval@gmail.com>
2015-11-25 15:42:49 +0100

    Add test skeleton and simple test for XcodeProg

---
+++ xcodeml-transform/src/test/xelement/XcodeProgTest.java
@@ -0,0 +1,27 @@
+package xelement;
+
+import java.io.File;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.w3c.dom.Element;
+import helper.XmlHelper;
+import x2x.translator.xcodeml.xelement.XcodeProg;
+
+public class XcodeProgTest {
+
+  // Path is relative to the test directory
+  private static final String TEST_DATA = "./data/basic.xcml";
+
+  @Test
+  public void basicXcodeProgTest() {
+    File f = new File(TEST_DATA);
+    assertTrue(f.exists());
+    XcodeProg xcodeml = new XcodeProg(TEST_DATA);
+    xcodeml.load();
+    xcodeml.readTypeTable();
+    xcodeml.readGlobalSymbolsTable();
+    assertEquals(8, xcodeml.getTypeTable().count());
+    assertEquals(2, xcodeml.getGlobalSymbolsTable().count());
+  }
+
+}
