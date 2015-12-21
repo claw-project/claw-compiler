919241e9c5de3cddd6dd1b0f9aa7fcf6d966914f
Valentin Clement <clementval@gmail.com>
2015-12-21 11:25:37 +0100

    Update tests to use clawf90 driver

--- test/base_test.cmake
+++ test/base_test.cmake
@@ -3,8 +3,6 @@
 
 # Define input and output file name
 set (ORIGINAL_FILE original_code.f90)
-set (XCODEML_FILE original_code.f90${XCODEML_EXT})
-set (XCODEML_CLAW_FILE original_code.f90${CLAW_EXT})
 set (OUTPUT_FILE transformed_code.f90)
 set (REFERENCE_FILE reference.f90)
 
@@ -14,26 +12,10 @@ set (EXECUTABLE_TRANSFORMED transformed_code_${TEST_NAME})
 
 # Create intermediate representation in XcodeML Fortran format
 add_custom_command(
-  OUTPUT  ${XCODEML_FILE}
-  COMMAND ${OMNI_F_FRONT} ${OMNI_F_FRONTEND_OPTION} -o ${XCODEML_FILE} ${ORIGINAL_FILE}
-  DEPENDS ${ORIGINAL_FILE}
-  COMMENT "Generating XcodeML file"
-)
-
-# Manipulate the intermediate representation and output XcodeML Fortran
-add_custom_command(
-  OUTPUT  ${XCODEML_CLAW_FILE}
-  COMMAND java -cp ${OMNIJAR_INCLUDE} ${OMNI_X2X} -xcodeml ${TRANSLATOR_OPTION} -o ${XCODEML_CLAW_FILE} ${XCODEML_FILE}
-  DEPENDS ${XCODEML_FILE}
-  COMMENT "Apply XcodeML transformation"
-)
-
-# Decompile XcodeML Fortran to standard Fortran
-add_custom_command(
   OUTPUT  ${OUTPUT_FILE}
-  COMMAND java -cp ${OMNIJAR_INCLUDE} ${OMNI_F_BACKEND} -l -w 80 -o ${OUTPUT_FILE} ${XCODEML_CLAW_FILE}
-  DEPENDS ${XCODEML_CLAW_FILE}
-  COMMENT "Decompile XcodeML to Fortran"
+  COMMAND ${CLAWF90} -o ${OUTPUT_FILE} ${ORIGINAL_FILE}
+  DEPENDS ${ORIGINAL_FILE}
+  COMMENT "Translating CLAW directive with ${CLAWF90}"
 )
 
 add_custom_target(transform-${TEST_NAME} ALL
@@ -44,7 +26,7 @@ add_custom_target(transform-${TEST_NAME} ALL
 add_custom_command(
   TARGET transform-${TEST_NAME}
   PRE_BUILD
-  COMMAND rm ${OUTPUT_FILE} ${XCODEML_FILE} ${XCODEML_CLAW_FILE}
+  COMMAND rm ${OUTPUT_FILE}
   COMMENT "Clean previous transformation"
 )
 
