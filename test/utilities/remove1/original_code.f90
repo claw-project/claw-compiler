92a8112682836a4b29fd430cd7a6de949ad5d0ff
Valentin Clement <clementval@gmail.com>
2015-12-29 10:50:04 +0100

    Add test for remove directive (with if, do block)

---
+++ test/utilities/remove1/original_code.f90
@@ -0,0 +1,25 @@
+! Simple program to test the remove directive
+
+PROGRAM remove1
+  CALL dummy
+END PROGRAM remove1
+
+SUBROUTINE dummy
+  INTEGER :: k,i
+  INTEGER :: kend = 5, iend = 10
+  DO k=1, kend
+    DO i=1, iend
+
+    END DO
+
+    !$claw remove
+    IF (k > 1) THEN
+      PRINT*, k
+    END IF
+
+    DO i=1, iend
+      ! loop #2 body here
+    END DO
+  END DO
+
+END SUBROUTINE dummy
