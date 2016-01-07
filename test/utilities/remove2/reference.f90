22f1fdd1fb0ceb94f2dc484dc2de0cf92146ec6d
Valentin Clement <clementval@gmail.com>
2016-01-07 11:40:23 +0100

    Add reference code for remove 2 example

---
+++ test/utilities/remove2/reference.f90
@@ -0,0 +1,19 @@
+PROGRAM remove2
+
+ CALL dummy ( )
+END PROGRAM remove2
+
+SUBROUTINE dummy ( )
+ INTEGER :: k
+ INTEGER :: i
+ INTEGER :: kend = 5
+ INTEGER :: iend = 10
+
+ DO k = 1 , kend , 1
+  DO i = 1 , iend , 1
+  END DO
+  DO i = 1 , iend , 1
+  END DO
+ END DO
+END SUBROUTINE dummy
+
