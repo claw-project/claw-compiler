01fd2c5eee8b2de25dd66b8c1477350700eef0fa
Valentin Clement <clementval@gmail.com>
2016-01-11 17:29:28 +0100

    Add test for logical and char constant in exprModel

--- xcodeml-transform/src/test/xelement/XexprModelTest.java
+++ xcodeml-transform/src/test/xelement/XexprModelTest.java
@@ -16,17 +16,37 @@ public class XexprModelTest {
   private String exprModel_IntConst = dummyRootOpen +
     "<FintConstant type=\"Fint\">16</FintConstant>" + dummyRootClose;
 
+  private String exprModel_LogConst = dummyRootOpen +
+    "<FlogicalConstant>0</FlogicalConstant>" + dummyRootClose; // TODO check with real XcodeML
+
+  private String exprModel_CharConst = dummyRootOpen +
+    "<FcharacterConstant type=\"C7fca03c0d3c0\">value1: </FcharacterConstant>"
+    + dummyRootClose;
 
 
   @Test
   public void findExprModelTest() {
     Document xml = XmlHelper.loadXMLFromString(exprModel_IntConst);
     XbaseElement element = new XbaseElement(xml.getDocumentElement());
-    System.out.println(exprModel_IntConst);
     assertNotNull(element);
     XexprModel model = XelementHelper.findExprModel(element);
     assertNotNull(model);
     assertTrue(model.isIntConst());
+
+
+    xml = XmlHelper.loadXMLFromString(exprModel_LogConst);
+    element = new XbaseElement(xml.getDocumentElement());
+    assertNotNull(element);
+    model = XelementHelper.findExprModel(element);
+    assertNotNull(model);
+    assertTrue(model.isLogicalConst());
+
+    xml = XmlHelper.loadXMLFromString(exprModel_CharConst);
+    element = new XbaseElement(xml.getDocumentElement());
+    assertNotNull(element);
+    model = XelementHelper.findExprModel(element);
+    assertNotNull(model);
+    assertTrue(model.isCharConst());
   }
 
 }
