487fa80072d08879873c3884f2af7435b44d95d7
Valentin Clement <clementval@gmail.com>
2016-01-11 17:32:30 +0100

    Add skeleton for XbaseExpr

---
+++ xcodeml-transform/src/x2x/translator/xcodeml/xelement/XbaseExpr.java
@@ -0,0 +1,19 @@
+package x2x.translator.xcodeml.xelement;
+
+import org.w3c.dom.Element;
+
+/**
+ * The XbaseExpr is a base class for the following elements
+ *
+ * Elements:
+ * - Required: exprModel (XexprModel), exprModel (XexprModel)
+ */
+
+public class XbaseExpr extends XbaseElement {
+  private XexprModel _part1 = null;
+  private XexprModel _part2 = null;
+
+  public XbaseExpr(Element element){
+    super(element);
+  }
+}
