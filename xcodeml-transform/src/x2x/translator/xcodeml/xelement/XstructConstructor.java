8612e35fee253e672b4d82b011d2cafc46511f5e
Valentin Clement <clementval@gmail.com>
2016-01-12 15:39:15 +0100

    Add skeleton for XarrayConstructor and XstructConstructor

---
+++ xcodeml-transform/src/x2x/translator/xcodeml/xelement/XstructConstructor.java
@@ -0,0 +1,28 @@
+package x2x.translator.xcodeml.xelement;
+
+import org.w3c.dom.Element;
+
+/**
+ * The XstructConstructor represents the FstructConstructor (7.3.1) element in
+ * XcodeML intermediate representation.
+ *
+ * Elements:
+ * - Optional:
+ *   - exprModel* (XexprModel) TODO
+ *
+ * Attributes:
+ * - Optional:
+ *   - type (text)
+ */
+
+public class XstructConstructor extends XbaseElement {
+
+  private String _type = null;
+
+  public XstructConstructor(Element baseElement){
+    super(baseElement);
+    _type = XelementHelper.getAttributeValue(baseElement,
+      XelementName.ATTR_TYPE);
+  }
+
+}
