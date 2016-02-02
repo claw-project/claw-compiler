a0b6fe7b055ba85225adee8ebaa5cdca1f2130fe
Valentin Clement <clementval@gmail.com>
2016-02-02 17:22:36 +0100

    Add new interface Xcreatable

---
+++ omni-cx2x/src/cx2x/xcodeml/xelement/Xcreatable.java
@@ -0,0 +1,17 @@
+package cx2x.xcodeml.xelement;
+
+/**
+ * Creatable interface defines methods that class that can be created must
+ * implement.
+ * @param <T> Derived class of XbaseElement
+ *
+ * @author clementval
+ */
+public interface Xcreatable<T extends XbaseElement> {
+
+  /**
+   * Construct an empty element of the defined type.
+   * @return A new object of type T that is empty.
+   */
+  T createEmpty();
+}
