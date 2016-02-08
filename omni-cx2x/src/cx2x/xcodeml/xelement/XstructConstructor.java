098afb20ee9f7dadca7aec1c5f5eec07d8339f0c
Valentin Clement <clementval@gmail.com>
2016-02-08 08:50:23 +0100

    Add exprModel list

--- omni-cx2x/src/cx2x/xcodeml/xelement/XstructConstructor.java
+++ omni-cx2x/src/cx2x/xcodeml/xelement/XstructConstructor.java
@@ -8,13 +8,16 @@ package cx2x.xcodeml.xelement;
 import org.w3c.dom.Element;
 import cx2x.xcodeml.helper.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The XstructConstructor represents the FstructConstructor (7.3.1) element in
  * XcodeML intermediate representation.
  *
  * Elements: ( exprModel )*
  * - Optional:
- *   - exprModel (XexprModel) TODO
+ *   - exprModel (XexprModel)
  *
  * Attributes:
  * - Optional:
@@ -26,6 +29,7 @@ import cx2x.xcodeml.helper.*;
 public class XstructConstructor extends XbaseElement {
 
   private String _type = null;
+  private List<XexprModel> _models = null;
 
   /**
    * Xelement standard ctor. Pass the base element to the base class and read
@@ -36,6 +40,8 @@ public class XstructConstructor extends XbaseElement {
     super(baseElement);
     _type = XelementHelper.getAttributeValue(this,
       XelementName.ATTR_TYPE);
+    _models = new ArrayList<>();
+    // TODO read all exprModel
   }
 
   /**
@@ -46,4 +52,12 @@ public class XstructConstructor extends XbaseElement {
     return _type;
   }
 
+
+  /**
+   * Get the list of inner expression models.
+   * @return A list of XexprModel objects. 
+   */
+  public List<XexprModel> getExprModels(){
+    return _models;
+  }
 }
