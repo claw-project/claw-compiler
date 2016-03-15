58b66cff11204dfe757bb082f81988068a362d9d
Valentin Clement <clementval@gmail.com>
2016-03-15 14:56:46 +0100

    Ignore generated ANTLR token files

---
+++ omni-cx2x/src/cx2x/translator/language/Claw.java
@@ -0,0 +1,12 @@
+/*
+ * This file is released under terms of BSD license
+ * See LICENSE file for more information
+ */
+
+package cx2x.translator.language;
+
+/**
+ * @author clementval
+ */
+public class Claw {
+}
