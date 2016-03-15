64f96d2e1e1b17ce9b6ebbf01e42688e8ed29d08
Valentin Clement <clementval@gmail.com>
2016-03-15 15:02:35 +0100

    Pushing enumeration for claw directive

---
+++ omni-cx2x/src/cx2x/translator/language/ClawDirective.java
@@ -0,0 +1,20 @@
+/*
+ * This file is released under terms of BSD license
+ * See LICENSE file for more information
+ */
+
+package cx2x.translator.language;
+
+/**
+ * ClawDirective enumeration represents the different directives available in
+ * the CLAW language.
+ *
+ * @author clementval
+ */
+public enum ClawDirective {
+  LOOP_FUSION,
+  LOOP_INTERCHANGE,
+  LOOP_EXTRACT,
+  REMOVE,
+  END_REMOVE
+}
