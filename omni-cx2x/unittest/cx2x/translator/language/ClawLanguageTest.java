8c9f11746bbdbacf3291792653363d4cdee058f1
Valentin Clement <clementval@gmail.com>
2016-03-16 14:31:10 +0100

    Add new junit class for the CLAW language parser + loop-fusion test

---
+++ omni-cx2x/unittest/cx2x/translator/language/ClawLanguageTest.java
@@ -0,0 +1,73 @@
+/*
+ * This file is released under terms of BSD license
+ * See LICENSE file for more information
+ */
+
+package cx2x.translator.language;
+
+import static org.junit.Assert.*;
+
+import cx2x.xcodeml.exception.IllegalDirectiveException;
+import org.junit.Test;
+
+/**
+ * JUnit test class for the CLAW language parser and information holding class.
+ *
+ * @author clementval
+ */
+public class ClawLanguageTest {
+
+  /**
+   * Test various input for the CLAW loop fusion directive.
+   */
+  @Test
+  public void FusionTest(){
+    // Valid directives
+    analyzeValidClawLoopFusion("claw loop-fusion", null);
+    analyzeValidClawLoopFusion("claw loop-fusion group(g1)", "g1");
+    analyzeValidClawLoopFusion("claw loop-fusion group( g1 )", "g1");
+    analyzeValidClawLoopFusion("claw loop-fusion group ( g1   ) ", "g1");
+    analyzeValidClawLoopFusion("claw loop-fusiongroup(g1)", "g1");
+
+    // Unvalid directives
+    analyzeUnvalidClawLanguage("claw loop-fusion group");
+    analyzeUnvalidClawLanguage("claw loop-fusion (i,j,k)");
+    analyzeUnvalidClawLanguage("claw loop-fusion group()");
+    analyzeUnvalidClawLanguage("claw loop-fusion group(   )");
+  }
+
+  /**
+   * Assert the result for valid loop fusion CLAW directive
+   * @param raw       Raw string valud of the CLAW directive to be analyzed.
+   * @param groupName Group name to be found if any.
+   */
+  private void analyzeValidClawLoopFusion(String raw, String groupName){
+    try {
+      ClawLanguage l = ClawLanguage.analyze(raw);
+      assertEquals(ClawDirective.LOOP_FUSION, l.getDirective());
+      if(groupName != null){
+        assertTrue(l.hasGroupOption());
+        assertEquals(groupName, l.getGroupName());
+      } else {
+        assertFalse(l.hasGroupOption());
+        assertNull(l.getGroupName());
+      }
+    } catch(IllegalDirectiveException idex){
+      fail();
+    }
+  }
+
+  /**
+   * Assert any unvalid claw raw input
+   * @param raw Raw string valud of the CLAW directive to be analyzed.
+   */
+  private void analyzeUnvalidClawLanguage(String raw){
+    ClawLanguage l = null;
+    try {
+      l = ClawLanguage.analyze(raw);
+      fail();
+    } catch (IllegalDirectiveException pex){
+      assertNull(l);
+    }
+  }
+}
