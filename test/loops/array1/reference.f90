84ece7430013654c3d054b94748f42896d353ebf
Valentin Clement <clementval@gmail.com>
2016-04-01 10:48:09 +0200

    update references

--- test/loops/array1/reference.f90
+++ test/loops/array1/reference.f90
@@ -1,18 +1,36 @@
 PROGRAM vector_loop
 
  CALL claw ( )
+ CALL claw_transformed ( )
 END PROGRAM vector_loop
 
 SUBROUTINE claw ( )
  INTEGER :: j
+ INTEGER :: vec1 ( 0 : 10 )
+ INTEGER :: claw_induction_0
+
+ DO j = 0 , 10 , 1
+  vec1 ( j ) = j
+ END DO
+!$claw array-transform
+ DO claw_induction_0 = 0 , 10
+  vec1 ( claw_induction_0 ) = vec1 ( claw_induction_0 ) + 10
+ END DO
+ PRINT * , vec1
+END SUBROUTINE claw
+
+SUBROUTINE claw_transformed ( )
  INTEGER :: i = 10
- INTEGER :: vec1 ( 0 : 9 )
+ INTEGER :: vec1 ( 0 : 10 )
+ INTEGER :: claw_induc1
+ INTEGER :: j
 
  DO j = 0 , i , 1
   vec1 ( j ) = j
  END DO
-!$claw loop-vector
- vec1 ( 0 : i ) = vec1 ( 0 : i ) + 10
+ DO claw_induc1 = 0 , i , 1
+  vec1 ( claw_induc1 ) = vec1 ( claw_induc1 ) + 10
+ END DO
  PRINT * , vec1
-END SUBROUTINE claw
+END SUBROUTINE claw_transformed
 
