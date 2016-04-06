fe619ccb0414a0c14a826b14a78cb2b63a950315
Valentin Clement <clementval@gmail.com>
2016-04-06 15:28:12 +0200

    Add test array6 for lower/upper bound checking

---
+++ test/openacc/array6/original_code.f90
@@ -0,0 +1,26 @@
+! Simple program to test the array notation to do loop directive
+! Test with var for lower/upper bound
+
+PROGRAM VECTOR_LOOP
+  CALL claw
+END
+
+! Before the transformation
+SUBROUTINE claw
+  INTEGER :: upper = 10
+  INTEGER :: lower = 0
+  INTEGER, DIMENSION(0:10) :: vec1
+  INTEGER, DIMENSION(0:10) :: vec2
+
+  vec1 = 0;
+  vec2 = 100;
+
+  !$claw array-transform
+  vec1(lower:upper) = vec2(lower:upper) + 10
+
+  !$claw array-transform
+  vec2(lower + 1:upper) = vec2(1) + 10
+
+  PRINT*,vec1
+  PRINT*,vec2
+END SUBROUTINE claw
---
+++ test/openacc/array6/reference.f90
@@ -0,0 +1,27 @@
+PROGRAM vector_loop
+
+ CALL claw ( )
+END PROGRAM vector_loop
+
+SUBROUTINE claw ( )
+ INTEGER :: upper = 10
+ INTEGER :: lower = 0
+ INTEGER :: vec1 ( 0 : 10 )
+ INTEGER :: vec2 ( 0 : 10 )
+ INTEGER :: claw_induction_0
+ INTEGER :: claw_induction_1
+
+ vec1 = 0
+ vec2 = 100
+!$claw array-transform
+ DO claw_induction_0 = lower , upper
+  vec1 ( claw_induction_0 ) = vec2 ( claw_induction_0 ) + 10
+ END DO
+!$claw array-transform
+ DO claw_induction_1 = lower + 1 , upper
+  vec2 ( claw_induction_1 ) = vec2 ( 1 ) + 10
+ END DO
+ PRINT * , vec1
+ PRINT * , vec2
+END SUBROUTINE claw
+
