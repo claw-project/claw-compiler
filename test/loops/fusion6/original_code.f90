0969833c6b10b51a2d0c38ea6f296c8c3f46109c
Valentin Clement <clementval@gmail.com>
2016-04-11 09:06:53 +0200

    Add test case for loop-fusion in different parent block level

---
+++ test/loops/fusion6/original_code.f90
@@ -0,0 +1,29 @@
+! Simple program to test the loop-fusion directive in different block level
+! the transformation should not occur as dirctive's parent block are different
+
+PROGRAM LOOP_FUSION
+  CALL clawloop
+END
+
+SUBROUTINE clawloop
+  INTEGER :: i
+  INTEGER :: j
+
+  !$claw loop-fusion group(i)
+  DO i=1,5
+    DO j=1,4
+      DO k=1,2
+        PRINT *, 'First loop body:',i,j
+      END DO
+    END DO
+  END DO
+
+  DO j=1,4
+    !$claw loop-fusion group(i)
+    DO i=1,5
+      DO k=1,2
+        PRINT *, 'Second loop body:',i,j
+      END DO
+    END DO
+  END DO
+END
---
+++ test/loops/fusion6/reference.f90
@@ -0,0 +1,28 @@
+PROGRAM loop_fusion
+
+ CALL clawloop ( )
+END PROGRAM loop_fusion
+
+SUBROUTINE clawloop ( )
+ INTEGER :: i
+ INTEGER :: j
+ INTEGER :: k
+
+!$claw loop-fusion group(i)
+ DO i = 1 , 5 , 1
+  DO j = 1 , 4 , 1
+   DO k = 1 , 2 , 1
+    PRINT * ,"First loop body:" , i , j
+   END DO
+  END DO
+ END DO
+ DO j = 1 , 4 , 1
+!$claw loop-fusion group(i)
+  DO i = 1 , 5 , 1
+   DO k = 1 , 2 , 1
+    PRINT * ,"Second loop body:" , i , j
+   END DO
+  END DO
+ END DO
+END SUBROUTINE clawloop
+
