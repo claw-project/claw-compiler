32cf3c2bf71d5db540903ad7559bfc2a442ee8fc
Valentin Clement <clementval@gmail.com>
2016-04-11 15:24:00 +0200

    Add method to create empty if statement with/w/o else block

--- omni-cx2x/src/cx2x/xcodeml/xelement/XifStatement.java
+++ omni-cx2x/src/cx2x/xcodeml/xelement/XifStatement.java
@@ -5,6 +5,7 @@
 
 package cx2x.xcodeml.xelement;
 
+import cx2x.xcodeml.exception.IllegalTransformationException;
 import org.w3c.dom.Element;
 import cx2x.xcodeml.helper.*;
 
@@ -43,15 +44,22 @@ public class XifStatement extends XenhancedElement {
    */
   public XifStatement(Element baseElement){
     super(baseElement);
+    readElementinformation();
+  }
+
+  /**
+   * Read inner element information.
+   */
+  private void readElementinformation(){
     _cond = XelementHelper.findCondition(this, false);
     _then = XelementHelper.findThen(this, false);
     _else = XelementHelper.findElse(this, false);
 
-
     // read optional attributes
     _construct_name = XelementHelper.getAttributeValue(this,
-      XelementName.ATTR_CONSTRUCT_NAME);
+        XelementName.ATTR_CONSTRUCT_NAME);
   }
+
 
 
   /**
@@ -92,5 +100,48 @@ public class XifStatement extends XenhancedElement {
    */
   public Xelse getElse(){
     return _else;
+  }
+
+  /**
+   * Create a new XifStatement object with minimal element (condition, then)
+   * @param xcodeml XcodeML program.
+   * @return A newly constructs XifStatement element with empty condition and
+   * block.
+   * @throws IllegalTransformationException can be thrown while constrcuting
+   * empty elements.
+   */
+  public static XifStatement create(XcodeProgram xcodeml)
+      throws IllegalTransformationException
+  {
+    XifStatement root = XelementHelper.createEmpty(XifStatement.class, xcodeml);
+    Xcondition cond = XelementHelper.createEmpty(Xcondition.class, xcodeml);
+    Xthen thenBlock = XelementHelper.createEmpty(Xthen.class, xcodeml);
+    Xbody thenBody = XelementHelper.createEmpty(Xbody.class, xcodeml);
+    thenBlock.appendToChildren(thenBody, false);
+    root.appendToChildren(thenBlock, false);
+    root.appendToChildren(cond, false);
+    root.readElementinformation();
+    return root;
+  }
+
+  /**
+   * Create a new XifStatement object with minimal blocks (condition, then,
+   * else)
+   * @param xcodeml XcodeML program.
+   * @return A newly constructs XifStatement element with empty condition and
+   * blocks.
+   * @throws IllegalTransformationException can be thrown while constrcuting
+   * empty elements.
+   */
+  public static XifStatement createWithElseBlock(XcodeProgram xcodeml)
+      throws IllegalTransformationException
+  {
+    XifStatement root = create(xcodeml);
+    Xelse elseBlock = XelementHelper.createEmpty(Xelse.class, xcodeml);
+    Xbody elseBody = XelementHelper.createEmpty(Xbody.class, xcodeml);
+    elseBlock.appendToChildren(elseBody, false);
+    root.appendToChildren(elseBlock, false);
+    root.readElementinformation();
+    return root;
   }
 }
