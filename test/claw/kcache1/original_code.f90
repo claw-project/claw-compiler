ea3de595171d57052061cd6836bee16005860a29
Valentin Clement <clementval@gmail.com>
2016-04-13 08:55:21 +0200

    Update test case to have correct use of cache for array9 0 -1

--- test/claw/kcache1/original_code.f90
+++ test/claw/kcache1/original_code.f90
@@ -11,13 +11,14 @@ SUBROUTINE kcache(istart,iend,jstart,jend)
   INTEGER, INTENT(IN) :: istart, iend, jstart, jend
   INTEGER :: i,j
   REAL(KIND=8), DIMENSION(istart:iend,istart:iend) :: array6, array7, array8, &
-                                                      array9
+                                                      array9, array10
 
   DO i = istart, iend
     array6(i,1) = 1.0
     array7(i,1) = 2.0
     array8(i,1) = 3.0
     array9(i,1) = 4.0
+    array10(i,1) = 4.0
   END DO
 
   DO i = istart, iend
@@ -30,6 +31,7 @@ SUBROUTINE kcache(istart,iend,jstart,jend)
       array8(i,j) = array8(i,j) * 2.0 + array6(i,j-1) + array7(i,j-1)
       !$claw kcache 0 -1
       array9(i,j) = array9(i,j) * 2.0 + array6(i,j-1) + array8(i,j-1)
+      array10(i,j) = array9(i,j-1) + 1.0
     END DO
   END DO
   PRINT*, SUM(array6)
--- test/claw/kcache1/reference.f90
+++ test/claw/kcache1/reference.f90
@@ -18,6 +18,7 @@ SUBROUTINE kcache ( istart , iend , jstart , jend )
  REAL ( KIND= 8 ) :: array7 ( istart : iend , istart : iend )
  REAL ( KIND= 8 ) :: array8 ( istart : iend , istart : iend )
  REAL ( KIND= 8 ) :: array9 ( istart : iend , istart : iend )
+ REAL ( KIND= 8 ) :: array10 ( istart : iend , istart : iend )
  REAL ( KIND= 8 ) :: array6_k_m1
  REAL ( KIND= 8 ) :: array7_k_m1
  REAL ( KIND= 8 ) :: array8_k_m1
@@ -28,6 +29,7 @@ SUBROUTINE kcache ( istart , iend , jstart , jend )
   array7 ( i , 1 ) = 2.0
   array8 ( i , 1 ) = 3.0
   array9 ( i , 1 ) = 4.0
+  array10 ( i , 1 ) = 4.0
  END DO
  DO i = istart , iend , 1
   DO j = jstart + 1 , jend , 1
@@ -39,6 +41,7 @@ SUBROUTINE kcache ( istart , iend , jstart , jend )
    array8 ( i , j ) = array8_k_m1
    array9_k_m1 = array9 ( i , j ) * 2.0 + array6_k_m1 + array8_k_m1
    array9 ( i , j ) = array9_k_m1
+   array10 ( i , j ) = array9_k_m1 + 1.0
   END DO
  END DO
  PRINT * , sum ( array6 )
