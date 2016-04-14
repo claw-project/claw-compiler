a3074c4834b7bd77c99bb9cc4dc1b16d00673884
Valentin Clement <clementval@gmail.com>
2016-04-14 16:31:56 +0200

    Add test case for block array-transform with two iter ranges

---
+++ test/openacc/array8/original_code.f90
@@ -0,0 +1,34 @@
+! Simple program to test the array notation to do loop directive
+! test block transformation of the array-transform directive
+
+PROGRAM array4_test
+  CALL claw_test
+END
+
+! Before the transformation
+SUBROUTINE claw_test
+  INTEGER, DIMENSION(0:10) :: vec1
+  INTEGER, DIMENSION(0:10) :: vec2
+  INTEGER, DIMENSION(0:10) :: vec3
+  INTEGER, DIMENSION(0:20) :: vec4
+  INTEGER, DIMENSION(0:20) :: vec5
+
+  vec1(:) = 0;
+  vec2(:) = 100;
+  vec4(:) = 10;
+  vec5(:) = 11;
+
+  !$claw array-transform
+  vec1(:) = vec2(:) + 10
+  vec2(:) = vec1(:) + 10
+
+  vec4(:) = vec5(:) + 1
+  vec4(:) = vec4(:) + 1
+  !$claw end array-transform
+
+  vec3(:) = vec1(:) + vec2(:)
+
+
+  PRINT*,vec1
+  PRINT*,vec2
+END SUBROUTINE claw_test
---
+++ test/openacc/array8/reference.f90
@@ -0,0 +1,31 @@
+PROGRAM array4_test
+
+ CALL claw_test ( )
+END PROGRAM array4_test
+
+SUBROUTINE claw_test ( )
+ INTEGER :: vec1 ( 0 : 10 )
+ INTEGER :: vec2 ( 0 : 10 )
+ INTEGER :: vec3 ( 0 : 10 )
+ INTEGER :: vec4 ( 0 : 20 )
+ INTEGER :: vec5 ( 0 : 20 )
+ INTEGER :: claw_induction_0
+ INTEGER :: claw_induction_1
+
+ vec1 ( 0 : 10 ) = 0
+ vec2 ( 0 : 10 ) = 100
+ vec4 ( 0 : 20 ) = 10
+ vec5 ( 0 : 20 ) = 11
+ DO claw_induction_0 = 0 , 10
+  vec1 ( claw_induction_0 ) = vec2 ( claw_induction_0 ) + 10
+  vec2 ( claw_induction_0 ) = vec1 ( claw_induction_0 ) + 10
+ END DO
+ DO claw_induction_1 = 0 , 20
+  vec4 ( claw_induction_1 ) = vec5 ( claw_induction_1 ) + 1
+  vec4 ( claw_induction_1 ) = vec4 ( claw_induction_1 ) + 1
+ END DO
+ vec3 ( 0 : 10 ) = vec1 ( 0 : 10 ) + vec2 ( 0 : 10 )
+ PRINT * , vec1
+ PRINT * , vec2
+END SUBROUTINE claw_test
+
