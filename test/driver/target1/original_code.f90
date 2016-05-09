6836a2b6c6083a5c34887a1cfde85f234da7f172
Valentin Clement <clementval@gmail.com>
2016-05-09 15:55:08 +0200

    Add test case for OpenACC macro

---
+++ test/driver/target1/original_code.f90
@@ -0,0 +1,26 @@
+! Simple program to test the loop-fusion directive with marco
+
+PROGRAM LOOP_FUSION
+  CALL clawloop
+END
+
+SUBROUTINE clawloop
+  INTEGER :: i
+  !$claw loop-fusion
+  DO i=1,10
+    PRINT *, 'First loop body:',i
+  END DO
+
+#ifdef _OPENACC
+  !$claw loop-fusion
+  DO i=1,10
+    PRINT *, 'Second loop body:',i
+  END DO
+#endif
+
+  !$claw loop-fusion
+  DO i=1,10
+    PRINT *, 'Third loop body:',i
+  END DO
+
+END
