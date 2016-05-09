605db390f7f31de9330e349958678d86056857e0
Valentin Clement <clementval@gmail.com>
2016-05-09 15:55:18 +0200

    Add test case for OpenMP macro

---
+++ test/driver/target2/original_code.f90
@@ -0,0 +1,26 @@
+! Simple program to test the loop-fusion directive with macro
+
+PROGRAM LOOP_FUSION
+  CALL clawloop
+END
+
+SUBROUTINE clawloop
+  INTEGER :: i
+  !$claw loop-fusion
+  DO i=1,10
+    PRINT *, 'First loop body:',i
+  END DO
+
+#ifdef _OPENMP
+  !$claw loop-fusion
+  DO i=1,10
+    PRINT *, 'Second loop body:',i
+  END DO
+#endif
+
+  !$claw loop-fusion
+  DO i=1,10
+    PRINT *, 'Third loop body:',i
+  END DO
+
+END
---
+++ test/driver/target2/reference.f90
@@ -0,0 +1,16 @@
+PROGRAM loop_fusion
+
+ CALL clawloop ( )
+END PROGRAM loop_fusion
+
+SUBROUTINE clawloop ( )
+ INTEGER :: i
+
+!$claw loop-fusion
+ DO i = 1 , 10 , 1
+  PRINT * ,"First loop body:" , i
+  PRINT * ,"Second loop body:" , i
+  PRINT * ,"Third loop body:" , i
+ END DO
+END SUBROUTINE clawloop
+
