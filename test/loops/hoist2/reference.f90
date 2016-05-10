df16e25c67d400606411c8bdd984c495ca5d77be
Valentin Clement <clementval@gmail.com>
2016-05-10 11:41:36 +0200

    Update test case reference

--- test/loops/hoist2/reference.f90
+++ test/loops/hoist2/reference.f90
@@ -15,8 +15,8 @@ SUBROUTINE claw_hoist2 ( )
  REAL :: var3
  REAL :: var4
  REAL :: var5
- REAL :: array2d_1 ( 1 : 10 , 1 : 100 )
- REAL :: array2d_2 ( 1 : 10 , 1 : 100 )
+ REAL :: array2d_1 ( 1 : 100 )
+ REAL :: array2d_2
  REAL :: array2d_3 ( 1 : 10 , 1 : 100 )
  REAL :: array2d_4 ( 1 : 10 , 1 : 100 )
  REAL :: array2d_5 ( 1 : 10 , 1 : 100 )
@@ -35,8 +35,8 @@ SUBROUTINE claw_hoist2 ( )
  ntrac = 2
  klev = 10
  kproma = 100
- array2d_1 ( 1 : 10 , 1 : 100 ) = 0.0
- array2d_2 ( 1 : 10 , 1 : 100 ) = 0.0
+ array2d_1 ( 1 : 100 ) = 0.0
+ array2d_2 = 0.0
  array2d_3 ( 1 : 10 , 1 : 100 ) = 0.0
  array2d_4 ( 1 : 10 , 1 : 100 ) = 0.0
  array2d_5 ( 1 : 10 , 1 : 100 ) = 0.0
@@ -50,20 +50,20 @@ SUBROUTINE claw_hoist2 ( )
     var1 = tdum % value1
     var2 = var1 ** ( 2. )
     IF ( jt == 2 ) THEN
-     array2d_2 ( i , j ) = min ( tdum % value2 * 2 , 50.e-6 )
+     array2d_2 = min ( tdum % value2 * 2 , 50.e-6 )
     END IF
-    array2d_1 ( i , j ) = 0.0
-    IF ( array2d_2 ( i , j ) > 0.0 ) THEN
+    array2d_1 ( j ) = 0.0
+    IF ( array2d_2 > 0.0 ) THEN
      var3 = array3d_1 ( i , j , jt ) + array3d_2 ( i , j , jt ) *&
       time_step_len
-     var5 = min ( 2.0 / 9.0 , array2d_1 ( i , j ) )
+     var5 = min ( 2.0 / 9.0 , array2d_1 ( j ) )
      zsedtend = min ( max ( 0.0 , var3 * var5 / array2d_3 ( i , j ) ) , 2.0 )
      array3d_2 ( i , j , jt ) = array3d_2 ( i , j , jt ) - var4
-     array2d_1 ( i , j ) = var4 * array2d_4 ( i , j )
+     array2d_1 ( j ) = var4 * array2d_4 ( i , j )
     END IF
     IF ( j >= 2 ) THEN
-     array3d_2 ( i , j , jt ) = array3d_2 ( i , j , jt ) + array2d_1 ( i , j -&
-      1 ) / array2d_5 ( i , j )
+     array3d_2 ( i , j , jt ) = array3d_2 ( i , j , jt ) + array2d_1 ( j - 1 )&
+      / array2d_5 ( i , j )
     END IF
    END DO
   END DO
