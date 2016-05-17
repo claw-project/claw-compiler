4ea9ecec1377bf35ad4b8b56c73f2c566ea60b2f
Valentin Clement <clementval@gmail.com>
2016-05-17 14:37:36 +0200

    Add javadoc

--- omni-cx2x/src/cx2x/translator/language/ClawDimension.java
+++ omni-cx2x/src/cx2x/translator/language/ClawDimension.java
@@ -18,7 +18,13 @@ public class ClawDimension {
   private final String _identifier;
 
 
-
+  /**
+   * Constructs a new dimension object from the extracted information.
+   * @param id          Identifier of the defined dimension.
+   * @param lowerBound  Lower bound of the dimension.
+   * @param upperBound  Upper bound of the dimension.
+   * TODO maybe add step information (in the grammar as well)
+   */
   public ClawDimension(String id, String lowerBound, String upperBound){
     _identifier = id;
 
@@ -47,31 +53,54 @@ public class ClawDimension {
   }
 
 
-
+  /**
+   * Check whether lower bound is an identifier variable.
+   * @return True if the lower bound is an identifier variable.
+   */
   public boolean lowerBoundIsVar(){
     return _lowerBoundId != null;
   }
 
+  /**
+   * Check whether upper bound is an identifier variable.
+   * @return True if the upper bound is an identifier variable.
+   */
   public boolean upperBoundIsVar(){
     return _upperBoundId != null;
   }
 
+  /**
+   * @return Lower bound value. -1 if lower bound is an identifier variable.
+   */
   public int getLowerBoundInt(){
     return _lowerBound;
   }
 
+  /**
+   * @return Upper bound value. -1 if upper bound is an identifier variable.
+   */
   public int getUpperBoundInt(){
     return _upperBound;
   }
 
+  /**
+   * @return Lower bound value. Null if lower bound is an integer constant.
+   */
   public String getLowerBoundId(){
     return _lowerBoundId;
   }
 
+  /**
+   * @return Upper bound value. Null if upper bound is an integer constant.
+   */
   public String getUpperBoundId(){
     return _upperBoundId;
   }
 
+  /**
+   * Get the identifier for the current dimension. 
+   * @returnThe identifier of the current dimension.
+   */
   public String getIdentifier(){
     return _identifier;
   }
