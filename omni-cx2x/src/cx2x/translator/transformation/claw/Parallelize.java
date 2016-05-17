a748667a5580949844b00abb6e3d2498a5ac0f80
Valentin Clement <clementval@gmail.com>
2016-05-17 14:37:53 +0200

    Push skeleton of parallelize transformation

---
+++ omni-cx2x/src/cx2x/translator/transformation/claw/Parallelize.java
@@ -0,0 +1,52 @@
+/*
+ * This file is released under terms of BSD license
+ * See LICENSE file for more information
+ */
+
+package cx2x.translator.transformation.claw;
+
+import cx2x.translator.language.ClawLanguage;
+import cx2x.xcodeml.transformation.Transformation;
+import cx2x.xcodeml.transformation.Transformer;
+import cx2x.xcodeml.xelement.XcodeProgram;
+
+/**
+ * The parallelize transformation transforms the code contained in a
+ * subtourine/function by adding necessary dimensions and parallelism to the
+ * defined data.
+ *
+ * @author clementval
+ */
+public class Parallelize extends Transformation {
+
+  private final ClawLanguage _claw;
+
+  /**
+   * Constructs a new Parallelize transfomration triggered from a specific
+   * pragma.
+   * @param directive The directive that triggered the k caching transformation.
+   */
+  public Parallelize(ClawLanguage directive) {
+    super(directive);
+    _claw = directive; // Keep information about the claw directive here
+  }
+
+  @Override
+  public boolean analyze(XcodeProgram xcodeml, Transformer transformer) {
+
+    return true;
+  }
+
+  @Override
+  public void transform(XcodeProgram xcodeml, Transformer transformer,
+                        Transformation other)
+      throws Exception
+  {
+
+  }
+
+  @Override
+  public boolean canBeTransformedWith(Transformation other) {
+    return false; // This is an independent transformation
+  }
+}
