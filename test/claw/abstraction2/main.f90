dc36e81cfe55f3bcb361ac50bf8fd37f48c10df8
Valentin Clement <clementval@gmail.com>
2016-05-18 08:51:27 +0200

    Push test case with two additional dimensions

---
+++ test/claw/abstraction2/main.f90
@@ -0,0 +1,25 @@
+! Test the CLAW abstraction model with two additional dimensions.
+PROGRAM test_abstraction2
+  USE mo_column, ONLY: compute_column
+
+  REAL, DIMENSION(10,10,60) :: q, t ! fields as declared in the whole model
+  INTEGER :: nx, ny, nz             ! Size of array fields
+  INTEGER :: i,j                    ! Loop indices
+
+  nx = 10
+  ny = 10
+  nz = 60
+
+#ifdef _CLAW
+  !CALL compute_column_claw(kend, q, t, xend, yend)
+#else
+  DO i = 1, xend
+    DO j = 1, yend
+      CALL compute_column(kend, q(i,j,:), t(i,j,:))
+    END DO
+  END DO
+#endif
+
+  PRINT*,SUM(q)
+  PRINT*,SUM(t)
+END PROGRAM test_abstraction2
---
+++ test/claw/abstraction2/mo_column.f90
@@ -0,0 +1,34 @@
+MODULE mo_column
+  IMPLICIT NONE
+
+CONTAINS
+
+  SUBROUTINE compute_column(nz, q, t)
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN)   :: nz   ! Size of the array field
+    REAL, INTENT(INOUT)   :: t(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: q(:) ! Field declared as one column only
+    INTEGER :: k                  ! Loop index
+    REAL :: c                     ! Coefficient
+
+    ! CLAW definition
+
+    ! Define two dimensions that will be added to the variables defined in the
+    ! data clause.
+    ! Apply the parallelization transformation on this subroutine.
+
+    !$claw define dimension i(1,nx) &
+    !$claw define dimension j(1,ny) &
+    !$claw parallelize data(q,t) over (i,j,:)
+
+    c = 5.345
+    DO k = 1, nz
+      t(k) = c * k
+      q(k) = q(k - 1)  + t(k) * c
+    END DO
+    q(kend) = q(kend) * c
+
+  END SUBROUTINE compute_column
+
+END MODULE mo_column
