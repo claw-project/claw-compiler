78ef82df760e5836f22714b9ddd62a556b5f5ac1
Valentin Clement <clementval@gmail.com>
2016-05-18 15:06:35 +0200

    Add default step value

--- omni-cx2x/src/cx2x/xcodeml/xelement/XelementName.java
+++ omni-cx2x/src/cx2x/xcodeml/xelement/XelementName.java
@@ -21,6 +21,7 @@ public class XelementName {
   public static final String FALSE = "false";
   public static final String SUPPORTED_VERSION = "1.0";
   public static final String SUPPORTED_LANGUAGE = "Fortran";
+  public static final String DEFAULT_STEP_VALUE = "1";
 
   // Base type (XcodeML/F 0.91J 9.1)
   public static final String TYPE_F_INT     = "Fint";
