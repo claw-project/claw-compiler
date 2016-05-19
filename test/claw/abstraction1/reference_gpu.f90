402216f064b2302fbaf6bee9c219477518480313
Valentin Clement <clementval@gmail.com>
2016-05-19 12:49:08 +0200

    Add gpu reference

---
+++ test/claw/abstraction1/reference_gpu.f90
@@ -0,0 +1,25 @@
+MODULE mo_column
+
+CONTAINS
+ SUBROUTINE compute_column ( nz , q , t , nproma )
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( : , : )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER :: k
+  REAL :: c
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: proma
+
+  DO proma = 1 , nproma , 1
+!$claw define dimension proma(1,nproma) claw parallelize data(q,t) over (proma,:)
+   c = 5.345
+   DO k = 1 , nz , 1
+    t ( nproma , k ) = c * k
+    q ( nproma , k ) = q ( nproma , k - 1 ) + t ( nproma , k ) * c
+   END DO
+   q ( nproma , nz ) = q ( nproma , nz ) * c
+  END DO
+ END SUBROUTINE compute_column
+
+END MODULE mo_column
+
