eda3adafda81a25457f70b1617b44e4cd26bbe1a
Valentin Clement <clementval@gmail.com>
2016-05-25 10:52:37 +0200

    Put module and program in a single file

--- test/claw/abstraction1/main.f90
+++ test/claw/abstraction1/main.f90
@@ -1,7 +1,37 @@
 ! Test the CLAW abstraction model with one additional dimension.
+MODULE mo_column
+  IMPLICIT NONE
+CONTAINS
+  ! Compute only one column
+  SUBROUTINE compute_column(nz, q, t)
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN)   :: nz   ! Size of the array field
+    REAL, INTENT(INOUT)   :: t(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: q(:) ! Field declared as one column only
+    INTEGER :: k                  ! Loop index
+    REAL :: c                     ! Coefficient
+
+    ! CLAW definition
+
+    ! Define one dimension that will be added to the variables defined in the
+    ! data clause.
+    ! Apply the parallelization transformation on this subroutine.
+
+    !$claw define dimension proma(1,nproma) &
+    !$claw parallelize data(q,t) over (proma,:)
+
+    c = 5.345
+    DO k = 2, nz
+      t(k) = c * k
+      q(k) = q(k - 1)  + t(k) * c
+    END DO
+    q(nz) = q(nz) * c
+  END SUBROUTINE compute_column
+END MODULE mo_column
+
 PROGRAM test_abstraction1
   USE mo_column, ONLY: compute_column
-
   REAL, DIMENSION(20,60) :: q, t  ! Fields as declared in the whole model
   INTEGER :: nproma, nz           ! Size of array fields
   INTEGER :: p                    ! Loop index
