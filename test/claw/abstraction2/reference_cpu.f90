e310d31f6a1dee9ba4cf50289269ad2996d24bc4
Valentin Clement <clementval@gmail.com>
2016-05-25 14:29:51 +0200

    Update reference with inner most + intermediate

--- test/claw/abstraction2/reference_cpu.f90
+++ test/claw/abstraction2/reference_cpu.f90
@@ -7,6 +7,7 @@ CONTAINS
   REAL , INTENT(INOUT) :: q ( : , : , : )
   INTEGER :: k
   REAL :: c
+  REAL :: d ( 1 : nx , 1 : ny )
   INTEGER , INTENT(IN) :: nx
   INTEGER :: i
   INTEGER , INTENT(IN) :: ny
@@ -14,14 +15,19 @@ CONTAINS
 
   c = 5.345
   DO k = 2 , nz , 1
-   DO i = 1 , nx , 1
-    DO j = 1 , ny , 1
+   DO j = 1 , ny , 1
+    DO i = 1 , nx , 1
      t ( i , j , k ) = c * k
     END DO
    END DO
-   DO i = 1 , nx , 1
-    DO j = 1 , ny , 1
-     q ( i , j , k ) = q ( i , j , k - 1 ) + t ( i , j , k ) * c
+   DO j = 1 , ny , 1
+    DO i = 1 , nx , 1
+     d( i , j ) = t ( i , j , k ) + c
+    END DO
+   END DO
+   DO j = 1 , ny , 1
+    DO i = 1 , nx , 1
+     q ( i , j , k ) = q ( i , j , k - 1 ) + t ( i , j , k ) * c + d ( i , j )
     END DO
    END DO
   END DO
