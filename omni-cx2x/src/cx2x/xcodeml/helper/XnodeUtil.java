d610fc6dc43e63325882975896bcf2e9f4a6534d
Valentin Clement <clementval@gmail.com>
2016-07-05 14:16:31 +0200

    Clean imports

--- omni-cx2x/src/cx2x/xcodeml/helper/XnodeUtil.java
+++ omni-cx2x/src/cx2x/xcodeml/helper/XnodeUtil.java
@@ -17,8 +17,6 @@ import org.w3c.dom.Node;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.dom.DOMSource;
