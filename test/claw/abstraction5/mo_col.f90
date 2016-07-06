797677c6e7d4368f5fbafdc924d7056737a9cf4a
Valentin Clement <clementval@gmail.com>
2016-07-06 10:39:33 +0200

    Push test case for parallelize forward

---
+++ test/claw/abstraction5/mo_col.f90
@@ -0,0 +1,17 @@
+MODULE mo_col
+  IMPLICIT NONE
+CONTAINS
+
+  SUBROUTINE compute(nz, q, t)
+    USE mo_column
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN)   :: nz   ! Size of the array field
+    REAL, INTENT(INOUT)   :: t(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: q(:) ! Field declared as one column only
+
+    !$claw parallelize forward
+    CALL compute_column(nz, q, t)
+  END SUBROUTINE compute
+
+END MODULE mo_col
