311c9fdbb3bde66ec8c88cd5ac5356ffc8ee09ca
Valentin Clement <clementval@gmail.com>
2016-07-11 14:09:42 +0200

    Add test case with several indirection and topological sorting

---
+++ test/claw/abstraction6/main.f90
@@ -0,0 +1,28 @@
+! Test the CLAW abstraction model with one additional dimension.
+PROGRAM test_abstraction6
+  USE mo_column_extra, ONLY: compute_one
+  REAL, DIMENSION(20,60) :: q, t  ! Fields as declared in the whole model
+  INTEGER :: nproma, nz           ! Size of array fields
+  INTEGER :: p                    ! Loop index
+
+  nproma = 20
+  nz = 60
+
+  DO p = 1, nproma
+    q(p,1) = 0.0
+    t(p,1) = 0.0
+  END DO
+
+  !$acc data copyin(q,t) copyout(q,t)
+
+  !$claw parallelize forward
+  DO p = 1, nproma
+    CALL compute_one(nz, q(p,:), t(p,:))
+  END DO
+
+
+  !$acc end data
+
+  PRINT*,SUM(q)
+  PRINT*,SUM(t)
+END PROGRAM test_abstraction6
