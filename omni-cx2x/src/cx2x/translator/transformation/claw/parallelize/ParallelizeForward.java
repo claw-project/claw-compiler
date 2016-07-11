0655a5c16fa578288418ab446dfeca5efdba7b32
Valentin Clement <clementval@gmail.com>
2016-07-11 11:39:24 +0200

    Implement flatten mode

--- omni-cx2x/src/cx2x/translator/transformation/claw/parallelize/ParallelizeForward.java
+++ omni-cx2x/src/cx2x/translator/transformation/claw/parallelize/ParallelizeForward.java
@@ -6,6 +6,7 @@ package cx2x.translator.transformation.claw.parallelize;
 
 import cx2x.translator.language.ClawLanguage;
 import cx2x.translator.language.helper.TransformationHelper;
+import cx2x.xcodeml.exception.IllegalTransformationException;
 import cx2x.xcodeml.helper.XnodeUtil;
 import cx2x.xcodeml.transformation.Transformation;
 import cx2x.xcodeml.transformation.Transformer;
@@ -29,6 +30,9 @@ public class ParallelizeForward extends Transformation {
   private boolean _flatten = false;
   private Xmod _mod = null;
 
+  private Xnode _innerDoStatement;
+  private Xnode _outerDoStatement;
+
   /**
    * Constructs a new Parallelize transformation triggered from a specific
    * pragma.
@@ -49,9 +53,10 @@ public class ParallelizeForward extends Transformation {
       return false;
     }
     if(next.Opcode() == Xcode.EXPRSTATEMENT){
-      return analyzeForward(xcodeml);
+      return analyzeForward(xcodeml, next);
     } else if (next.Opcode() == Xcode.FDOSTATEMENT) {
-      return analyzeForwardWithDo(xcodeml);
+      _outerDoStatement = next;
+      return analyzeForwardWithDo(xcodeml, next);
     }
     xcodeml.addError("Directive is not followed a valid statement.",
         _claw.getPragma().getLineNo());
@@ -61,28 +66,68 @@ public class ParallelizeForward extends Transformation {
   /**
    * Analyze the directive when it is used just before a do statement.
    * @param xcodeml Current XcodeML file unit.
+   * @param doStmt  The do statement following the pragma.
    * @return True if the analysis succeed. False otherwise.
    */
-  private boolean analyzeForwardWithDo(XcodeProgram xcodeml){
+  private boolean analyzeForwardWithDo(XcodeProgram xcodeml, Xnode doStmt){
     _flatten = true;
-    return true;
+    if(doStmt == null){
+      xcodeml.addError("Directive is not followed by do statement.",
+          _claw.getPragma().getLineNo());
+      return false;
+    }
+
+    // Try to locate the fct call inside of the do statements. Can be nested.
+    return analyzeNestedDoStmts(xcodeml, doStmt);
+  }
+
+  /**
+   *
+   * @param xcodeml
+   * @param doStmt
+   * @return
+   */
+  private boolean analyzeNestedDoStmts(XcodeProgram xcodeml, Xnode doStmt){
+    _innerDoStatement = doStmt;
+    Xnode body = doStmt.find(Xcode.BODY);
+    if(body == null){
+      xcodeml.addError("Cannot locate function call.",
+          _claw.getPragma().getLineNo());
+      return false;
+    }
+    for(Xnode n : body.getChildren()){
+      if(n.Opcode() == Xcode.FDOSTATEMENT){
+        return analyzeNestedDoStmts(xcodeml, doStmt);
+      } else if(n.Opcode() != Xcode.FPRAGMASTATEMENT
+          && n.Opcode() != Xcode.EXPRSTATEMENT)
+      {
+        xcodeml.addError("Only pragmas, comments and function calls allowed " +
+                "in the do statements.",
+            _claw.getPragma().getLineNo());
+        return false;
+      } else if (n.Opcode() == Xcode.EXPRSTATEMENT){
+        return analyzeForward(xcodeml, n);
+      }
+    }
+    xcodeml.addError("Function call not found.", _claw.getPragma().getLineNo());
+    return false;
   }
 
 
   /**
    * Analyze the directive when it is used just before a function call.
    * @param xcodeml Current XcodeML file unit.
+   * @param expr    The expression element following the pragma.
    * @return True if the analysis succeed. False otherwise.
    */
-  private boolean analyzeForward(XcodeProgram xcodeml){
-    Xnode ex = XnodeUtil.findDirectNext(Xcode.EXPRSTATEMENT, _claw.getPragma());
-    if(ex == null){
+  private boolean analyzeForward(XcodeProgram xcodeml, Xnode expr){
+    if(expr == null){
       xcodeml.addError("Directive is not followed by a fct call.",
           _claw.getPragma().getLineNo());
       return false;
     }
 
-    _fctCall = ex.find(Xcode.FUNCTIONCALL);
+    _fctCall = expr.find(Xcode.FUNCTIONCALL);
     if(_fctCall == null){
       xcodeml.addError("Directive is not followed by a fct call.",
           _claw.getPragma().getLineNo());
@@ -100,9 +145,12 @@ public class ParallelizeForward extends Transformation {
 
       XfunctionDefinition parentFctDef =
           XnodeUtil.findParentFunction(_claw.getPragma());
-      List<Xdecl> uses = parentFctDef.getDeclarationTable().getAll(Xcode.FUSEDECL);
+      List<Xdecl> allUses =
+          parentFctDef.getDeclarationTable().getAll(Xcode.FUSEDECL);
+      allUses.addAll(parentFctDef.getDeclarationTable().
+          getAll(Xcode.FUSEONLYDECL));
 
-      for(Xdecl d : uses){
+      for(Xdecl d : allUses){
 
         // Check whether a CLAW file is available.
         _mod = TransformationHelper.
@@ -153,7 +201,9 @@ public class ParallelizeForward extends Transformation {
   private void transformFlatten(XcodeProgram xcodeml, Transformer transformer)
       throws Exception
   {
-
+    XnodeUtil.extractBody(_innerDoStatement, _outerDoStatement);
+    _outerDoStatement.delete();
+    transformStd(xcodeml, transformer);
   }
 
   /**
@@ -179,7 +229,13 @@ public class ParallelizeForward extends Transformation {
       Xnode p = params.get(i);
       String var = p.getValue();
       String type;
+
       if(!fDef.getSymbolTable().contains(var)){
+        if(_flatten){
+          throw new IllegalTransformationException("Variable " + var + " must" +
+              " be locally defined where the last call to parallelize if made.",
+              _claw.getPragma().getLineNo());
+        }
         // Size variable have to be declared
         XbasicType intTypeIntentIn = XnodeUtil.createBasicType(xcodeml,
             xcodeml.getTypeTable().generateIntegerTypeHash(),
@@ -190,48 +246,73 @@ public class ParallelizeForward extends Transformation {
         type = intTypeIntentIn.getType();
         XnodeUtil.createAndAddParam(xcodeml, var, type, parentFctType);
       } else {
-        // Var exists already. Add to to the parameters if not here.
+        // Var exists already. Add to the parameters if not here.
         type = fDef.getSymbolTable().get(var).getType();
-        XnodeUtil.
-            createAndAddParamIfNotExists(xcodeml, var, type, parentFctType);
+
+        /* If flatten mode, we do not add extra parameters to the function
+         * definition */
+        if(!_flatten) {
+          XnodeUtil.
+              createAndAddParamIfNotExists(xcodeml, var, type, parentFctType);
+        }
       }
 
+      // Add variable in the function call
       Xnode arg = XnodeUtil.createVar(type, var, Xscope.LOCAL, xcodeml);
       _fctCall.find(Xcode.ARGUMENTS).appendToChildren(arg, false);
     }
 
-    // 2. Adapt function/subroutine in which the function call is nested
-    for(Xnode pBase : _fctType.getParams().getAll()){
-      for(Xnode pUpdate : parentFctType.getParams().getAll()){
-        if(pBase.getValue().equals(pUpdate.getValue())){
-          XbasicType typeBase = (_localFct) ? (XbasicType)
-              xcodeml.getTypeTable().get(pBase.getAttribute(Xattr.TYPE)) :
-              (XbasicType) _mod.getTypeTable().get(pBase.getAttribute(Xattr.TYPE));
-          XbasicType typeToUpdate = (XbasicType)xcodeml.getTypeTable().get(pUpdate.getAttribute(Xattr.TYPE));
+    // In flatten mode, arguments are demoted if needed.
+    if(_flatten){
+      Xnode arguments = _fctCall.find(Xcode.ARGUMENTS);
+      for(Xnode arg : arguments.getChildren()){
+        if(arg.Opcode() == Xcode.FARRAYREF
+            && arg.find(Xcode.INDEXRANGE) != null)
+        {
+          Xnode var = arg.find(Xcode.VARREF, Xcode.VAR);
+          if(var != null){
+            XnodeUtil.insertAfter(arg, var.cloneObject());
+            arg.delete();
+          }
+        }
+      }
+    } else {
+      // 2. Adapt function/subroutine in which the function call is nested
+      for(Xnode pBase : _fctType.getParams().getAll()){
+        for(Xnode pUpdate : parentFctType.getParams().getAll()){
+          if(pBase.getValue().equals(pUpdate.getValue())){
+            XbasicType typeBase = (_localFct) ? (XbasicType)
+                xcodeml.getTypeTable().get(pBase.getAttribute(Xattr.TYPE)) :
+                (XbasicType) _mod.getTypeTable().
+                    get(pBase.getAttribute(Xattr.TYPE));
+            XbasicType typeToUpdate = (XbasicType)xcodeml.getTypeTable().
+                get(pUpdate.getAttribute(Xattr.TYPE));
 
-          // Types have different dimensions
-          if(typeBase.getDimensions() > typeToUpdate.getDimensions()){
-            String type = XnodeUtil.duplicateWithDimension(typeBase,
-                typeToUpdate, xcodeml);
-            pUpdate.setAttribute(Xattr.TYPE, type);
+            // Types have different dimensions
+            if(typeBase.getDimensions() > typeToUpdate.getDimensions()){
+              String type = XnodeUtil.duplicateWithDimension(typeBase,
+                  typeToUpdate, xcodeml);
+              pUpdate.setAttribute(Xattr.TYPE, type);
 
-            Xid id = fDef.getSymbolTable().get(pBase.getValue());
-            if(id != null){
-              id.setAttribute(Xattr.TYPE, type);
-            }
-            Xdecl varDecl = fDef.getDeclarationTable().get(pBase.getValue());
-            if(varDecl != null){
-              varDecl.find(Xcode.NAME).setAttribute(Xattr.TYPE, type);
+              Xid id = fDef.getSymbolTable().get(pBase.getValue());
+              if(id != null){
+                id.setAttribute(Xattr.TYPE, type);
+              }
+              Xdecl varDecl = fDef.getDeclarationTable().get(pBase.getValue());
+              if(varDecl != null){
+                varDecl.find(Xcode.NAME).setAttribute(Xattr.TYPE, type);
+              }
             }
           }
         }
       }
+
+      // 3. Replicate the change in a potential module file
+      XmoduleDefinition modDef = XnodeUtil.findParentModule(fDef);
+      XnodeUtil.updateModuleSignature(xcodeml, fDef, parentFctType, modDef,
+          _claw, transformer);
     }
 
-    // 3. Replicate the change in a potential module file
-    XmoduleDefinition modDef = XnodeUtil.findParentModule(fDef);
-    XnodeUtil.updateModuleSignature(xcodeml, fDef, parentFctType, modDef, _claw,
-        transformer);
   }
 
   @Override
