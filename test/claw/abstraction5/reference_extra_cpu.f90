06c9f0781c6a82f93c2af706f4ce111c50218d97
Valentin Clement <clementval@gmail.com>
2016-07-11 10:06:59 +0200

    Add references with the test case

---
+++ test/claw/abstraction5/reference_extra_cpu.f90
@@ -0,0 +1,15 @@
+MODULE mo_column_extra
+
+CONTAINS
+ SUBROUTINE compute ( nz , q , t , nproma )
+  USE mo_column
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( : , : )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER , INTENT(IN) :: nproma
+
+  CALL compute_column ( nz , q , t , nproma )
+ END SUBROUTINE compute
+
+END MODULE mo_column_extra
+
