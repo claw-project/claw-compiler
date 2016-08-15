59a9b18be084dc57e93a39ee0cc8110ad143b425
Valentin Clement <clementval@gmail.com>
2016-08-15 09:02:40 +0200

    Add reference for ignore1

---
+++ test/driver/ignore1/reference.f90
@@ -0,0 +1,9 @@
+PROGRAM testignore
+  !$claw remove
+  PRINT*,'These lines'
+  PRINT*,'are ignored'
+  PRINT*,'by the CLAW compiler'
+  PRINT*,'but kept in the final transformed code'
+  PRINT*,'with the remove directives.'
+  !$claw end remove
+END PROGRAM testignore
