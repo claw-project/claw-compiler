d64ff21fc000322124b9e08ae965573015be4a06
Valentin Clement <clementval@gmail.com>
2016-08-18 11:12:47 +0200

    Add reference files from abstraction6

---
+++ test/claw/abstraction8/reference_gpu.f90
@@ -0,0 +1,36 @@
+MODULE mo_column
+
+CONTAINS
+ SUBROUTINE compute_solver ( nz , q , t , nproma )
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( : , : )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER , INTENT(IN) :: nproma
+
+  CALL compute_column ( nz , q , t , nproma = nproma )
+ END SUBROUTINE compute_solver
+
+ SUBROUTINE compute_column ( nz , q , t , nproma )
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( : , : )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER :: k
+  REAL :: c
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: proma
+
+!$acc parallel
+!$acc loop
+  DO proma = 1 , nproma , 1
+   c = 5.345
+   DO k = 2 , nz , 1
+    t ( proma , k ) = c * k
+    q ( proma , k ) = q ( proma , k - 1 ) + t ( proma , k ) * c
+   END DO
+   q ( proma , nz ) = q ( proma , nz ) * c
+  END DO
+!$acc end parallel
+ END SUBROUTINE compute_column
+
+END MODULE mo_column
+
