152ec186734aa3bb0a3901b515d141126880ba11
Valentin Clement <clementval@gmail.com>
2016-08-19 10:25:00 +0200

    Add references files

---
+++ test/claw/abstraction9/reference_cpu.f90
@@ -0,0 +1,38 @@
+MODULE mo_column
+
+CONTAINS
+ SUBROUTINE compute_column_public ( nz , q , t , nproma )
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( : , : )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER :: r
+  INTEGER , INTENT(IN) :: nproma
+
+  CALL compute_column ( nz , q , t , nproma = nproma )
+ END SUBROUTINE compute_column_public
+
+ SUBROUTINE compute_column ( nz , q , t , nproma )
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( : , : )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER :: k
+  REAL :: c
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: proma
+
+  c = 5.345
+  DO k = 2 , nz , 1
+   DO proma = 1 , nproma , 1
+    t ( proma , k ) = c * k
+   END DO
+   DO proma = 1 , nproma , 1
+    q ( proma , k ) = q ( proma , k - 1 ) + t ( proma , k ) * c
+   END DO
+  END DO
+  DO proma = 1 , nproma , 1
+   q ( proma , nz ) = q ( proma , nz ) * c
+  END DO
+ END SUBROUTINE compute_column
+
+END MODULE mo_column
+
