690cdaec2d2fef008f5f291b33960872f34dcd1d
Valentin Clement <clementval@gmail.com>
2016-08-22 15:09:58 +0200

    Add new test case for different over clause

---
+++ test/claw/abstraction13/main.f90
@@ -0,0 +1,33 @@
+! Test the CLAW abstraction model with one additional dimension.
+PROGRAM test_abstraction12
+  USE mo_column, ONLY: compute
+  REAL, DIMENSION(20,60) :: q     ! Fields as declared in the whole model
+  REAL, DIMENSION(60,20) :: t     ! Fields as declared in the whole model
+  INTEGER :: nproma, nz           ! Size of array fields
+  INTEGER :: p                    ! Loop index
+  INTEGER :: b
+
+  nproma = 20
+  nz = 60
+  b = 20
+
+  DO p = 1, nproma
+    q(p,1) = 0.0
+  END DO
+
+  DO p = 1, nproma
+    t(1,p) = 0.0
+  END DO
+
+  !$acc data copyin(q,t) copyout(q,t)
+
+  !$claw parallelize forward
+  DO p = 1, nproma
+    CALL compute(nz, b, q(p,:), t(:,p))
+  END DO
+
+  !$acc end data
+
+  PRINT*,SUM(q)
+  PRINT*,SUM(t)
+END PROGRAM test_abstraction12
