1e8cb6e36971e7f8bdad8ddf7d6c1365ae80d064
Valentin Clement <clementval@gmail.com>
2016-08-22 13:08:42 +0200

    Add references

---
+++ test/claw/abstraction12/reference_cpu.f90
@@ -0,0 +1,41 @@
+MODULE mo_column
+
+CONTAINS
+ FUNCTION compute_column ( nz , b , q , t , nproma ) RESULT(r)
+  INTEGER :: r
+  INTEGER , INTENT(IN) :: nz
+  INTEGER , INTENT(IN) :: b
+  REAL , INTENT(INOUT) :: t ( 1 : b , 1 : nproma )
+  REAL , INTENT(INOUT) :: q ( 1 : b , 1 : nproma )
+  INTEGER :: k
+  REAL :: c
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: proma
+
+  c = 5.345
+  DO k = 2 , nz , 1
+   DO proma = 1 , nproma , 1
+    t ( k , proma ) = c * k
+   END DO
+   DO proma = 1 , nproma , 1
+    q ( k , proma ) = q ( k - 1 , proma ) + t ( k , proma ) * c
+   END DO
+  END DO
+  DO proma = 1 , nproma , 1
+   q ( nz , proma ) = q ( nz , proma ) * c
+  END DO
+ END FUNCTION compute_column
+
+ SUBROUTINE compute ( nz , b , q , t , nproma )
+  INTEGER , INTENT(IN) :: nz
+  INTEGER , INTENT(IN) :: b
+  REAL , INTENT(INOUT) :: t ( 1 : b , 1 : nproma )
+  REAL , INTENT(INOUT) :: q ( 1 : b , 1 : nproma )
+  INTEGER :: result
+  INTEGER , INTENT(IN) :: nproma
+
+  result = compute_column ( nz , b , q , t , nproma = nproma )
+ END SUBROUTINE compute
+
+END MODULE mo_column
+
---
+++ test/claw/abstraction12/reference_gpu.f90
@@ -0,0 +1,40 @@
+MODULE mo_column
+
+CONTAINS
+ FUNCTION compute_column ( nz , b , q , t , nproma ) RESULT(r)
+  INTEGER :: r
+  INTEGER , INTENT(IN) :: nz
+  INTEGER , INTENT(IN) :: b
+  REAL , INTENT(INOUT) :: t ( 1 : b , 1 : nproma )
+  REAL , INTENT(INOUT) :: q ( 1 : b , 1 : nproma )
+  INTEGER :: k
+  REAL :: c
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: proma
+
+!$acc parallel
+!$acc loop
+  DO proma = 1 , nproma , 1
+   c = 5.345
+   DO k = 2 , nz , 1
+    t ( k , proma ) = c * k
+    q ( k , proma ) = q ( k - 1 , proma ) + t ( k , proma ) * c
+   END DO
+   q ( nz , proma ) = q ( nz , proma ) * c
+  END DO
+!$acc end parallel
+ END FUNCTION compute_column
+
+ SUBROUTINE compute ( nz , b , q , t , nproma )
+  INTEGER , INTENT(IN) :: nz
+  INTEGER , INTENT(IN) :: b
+  REAL , INTENT(INOUT) :: t ( 1 : b , 1 : nproma )
+  REAL , INTENT(INOUT) :: q ( 1 : b , 1 : nproma )
+  INTEGER :: result
+  INTEGER , INTENT(IN) :: nproma
+
+  result = compute_column ( nz , b , q , t , nproma = nproma )
+ END SUBROUTINE compute
+
+END MODULE mo_column
+
