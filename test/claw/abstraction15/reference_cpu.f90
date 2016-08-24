d2915d41a3f9aa8b2b82d3cb3dc74199c279de0f
Valentin Clement <clementval@gmail.com>
2016-08-24 14:33:11 +0200

    Add references

---
+++ test/claw/abstraction15/reference_cpu.f90
@@ -0,0 +1,43 @@
+MODULE mo_column
+
+CONTAINS
+ FUNCTION compute_column ( nz , b , q , t , z , nproma ) RESULT(r)
+  INTEGER :: r
+  INTEGER , INTENT(IN) :: nz
+  INTEGER , INTENT(IN) :: b
+  REAL , INTENT(INOUT) :: t ( 1 : nproma , 1 : b )
+  REAL , INTENT(INOUT) :: q ( 1 : b , 1 : nproma )
+  REAL , INTENT(IN) :: z ( : )
+  INTEGER :: k
+  REAL :: c
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: proma
+
+  c = 5.345
+  DO k = 2 , nz , 1
+   DO proma = 1 , nproma , 1
+    t ( proma , k ) = c * k
+   END DO
+   DO proma = 1 , nproma , 1
+    q ( k , proma ) = q ( k - 1 , proma ) + t ( proma , k ) * c
+   END DO
+  END DO
+  DO proma = 1 , nproma , 1
+   q ( nz , proma ) = q ( nz , proma ) * c
+  END DO
+ END FUNCTION compute_column
+
+ SUBROUTINE compute ( nz , b , q , t , z , nproma )
+  INTEGER , INTENT(IN) :: nz
+  INTEGER , INTENT(IN) :: b
+  REAL , INTENT(INOUT) :: t ( 1 : nproma , 1 : b )
+  REAL , INTENT(INOUT) :: q ( 1 : b , 1 : nproma )
+  REAL , INTENT(IN) :: z ( : )
+  INTEGER :: result
+  INTEGER , INTENT(IN) :: nproma
+
+  result = compute_column ( nz , b , q , t , z , nproma = nproma )
+ END SUBROUTINE compute
+
+END MODULE mo_column
+
