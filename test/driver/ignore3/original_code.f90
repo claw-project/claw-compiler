1e1bcd7d593ae940bf430e9acb1a85364cdca5e1
Valentin Clement <clementval@gmail.com>
2016-08-31 15:00:53 +0200

    Add test case for ignore clause with continuation line

---
+++ test/driver/ignore3/original_code.f90
@@ -0,0 +1,19 @@
+PROGRAM testignore
+
+  !$claw ignore
+  REAL :: real_var_1, &
+          real_var_2, &
+          real_var_3, &
+          real_var_4, &
+          real_var_5, &
+          real_var_6, &
+          real_var_7, &
+          real_var_8, &
+          real_var_9, &
+          real_var_10, &
+          real_var_11, &
+          real_var_12, &
+          real_var_13
+  !$claw end ignore
+
+END PROGRAM testignore
