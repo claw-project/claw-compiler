2d376220d371f52ab652afd1864081f93aebe820
Valentin Clement <clementval@gmail.com>
2016-09-07 10:03:27 +0200

    Add references

---
+++ test/claw/abstraction22/reference_cpu.f90
@@ -0,0 +1,47 @@
+MODULE mo_column
+
+CONTAINS
+ SUBROUTINE compute_all ( nz , q , t , nproma )
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( : , : )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  REAL , TARGET :: z ( 1 : nproma , 1 : nz )
+  INTEGER :: k
+  REAL , POINTER :: zp ( : , : )
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: iter_nproma
+
+  DO k = 1 , nz , 1
+   DO iter_nproma = 1 , nproma , 1
+    z ( iter_nproma , k ) = t ( k , iter_nproma ) + q ( iter_nproma , k )
+   END DO
+  END DO
+  zp => z
+  CALL compute_column ( nz , q , t , nproma = nproma )
+ END SUBROUTINE compute_all
+
+ SUBROUTINE compute_column ( nz , q , t , nproma )
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( : , : )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER :: k
+  REAL :: c
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: proma
+
+  c = 5.345
+  DO k = 2 , nz , 1
+   DO proma = 1 , nproma , 1
+    t ( k , proma ) = c * k
+   END DO
+   DO proma = 1 , nproma , 1
+    q ( proma , k ) = t ( k - 1 , proma ) + t ( k , proma ) * c
+   END DO
+  END DO
+  DO proma = 1 , nproma , 1
+   q ( proma , nz ) = q ( proma , nz ) * c
+  END DO
+ END SUBROUTINE compute_column
+
+END MODULE mo_column
+
