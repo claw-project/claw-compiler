700693f691392eedc888001cfe7df0763f93757f
Valentin Clement <clementval@gmail.com>
2016-09-07 14:19:37 +0200

    Add test case for contains section remove

---
+++ test/utilities/remove3/original_code.f90
@@ -0,0 +1,26 @@
+! Simple program to test the remove directive
+
+PROGRAM remove3
+  CALL dummy
+END PROGRAM remove3
+
+SUBROUTINE dummy
+  INTEGER :: k,i
+  INTEGER :: kend = 5, iend = 10
+  DO k=1, kend
+    DO i=1, iend
+      ! loop #1 body here
+    END DO
+
+    DO i=1, iend
+      ! loop #2 body here
+    END DO
+  END DO
+
+  !$claw remove
+  CONTAINS
+    SUBROUTINE dummy_inside
+
+    END SUBROUTINE
+
+END SUBROUTINE dummy
