510bb3867435679a73ff134907de86ea1e0b8ac5
Valentin Clement <clementval@gmail.com>
2016-09-08 11:39:19 +0200

    Add test case for namedValue arg in foward

---
+++ test/claw/abstraction24/main.f90
@@ -0,0 +1,28 @@
+! Test the CLAW abstraction model with one additional dimension.
+PROGRAM test_abstraction24
+  USE mo_column, ONLY: compute_all
+  REAL, DIMENSION(20,60) :: q  ! Fields as declared in the whole model
+  REAL, DIMENSION(60,20) :: t  ! Fields as declared in the whole model
+  INTEGER :: nproma, nz           ! Size of array fields
+  INTEGER :: p                    ! Loop index
+
+  nproma = 20
+  nz = 60
+
+  DO p = 1, nproma
+    q(p,1) = 0.0
+    t(1,p) = 0.0
+  END DO
+
+  !$acc data copyin(q,t) copyout(q,t)
+
+  !$claw parallelize forward
+  DO p = 1, nproma
+    CALL compute_all(nz, q(p,:), t(:,p))
+  END DO
+
+  !$acc end data
+
+  PRINT*,SUM(q)
+  PRINT*,SUM(t)
+END PROGRAM test_abstraction24
