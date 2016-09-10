01993e87d9d31a4368bdafcf8626ed5913e19d5f
Valentin Clement <clementval@gmail.com>
2016-09-10 15:07:06 +0200

    Add refs

---
+++ test/claw/abstraction25/reference_cpu.f90
@@ -0,0 +1,39 @@
+MODULE mo_column
+
+CONTAINS
+ FUNCTION compute_column ( nz , q , t , nproma ) RESULT(r)
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( 1 : nz , 1 : nproma )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER :: k
+  REAL :: c
+  INTEGER :: r
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: proma
+
+  c = 5.345
+  DO k = 2 , nz , 1
+   DO proma = 1 , nproma , 1
+    t ( k , proma ) = c * k
+   END DO
+   DO proma = 1 , nproma , 1
+    q ( k , proma ) = q ( k - 1 , proma ) + t ( k , proma ) * c
+   END DO
+  END DO
+  DO proma = 1 , nproma , 1
+   q ( nz , proma ) = q ( nz , proma ) * c
+  END DO
+ END FUNCTION compute_column
+
+ SUBROUTINE compute ( nz , q , nproma )
+  INTEGER , INTENT(IN) :: nz
+  REAL :: t ( 1 : nz , 1 : nproma )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER :: result
+  INTEGER , INTENT(IN) :: nproma
+
+  result = compute_column ( nz , q , t , nproma = nproma )
+ END SUBROUTINE compute
+
+END MODULE mo_column
+
---
+++ test/claw/abstraction25/reference_gpu.f90
@@ -0,0 +1,38 @@
+MODULE mo_column
+
+CONTAINS
+ FUNCTION compute_column ( nz , q , t , nproma ) RESULT(r)
+  INTEGER , INTENT(IN) :: nz
+  REAL , INTENT(INOUT) :: t ( 1 : nz , 1 : nproma )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER :: k
+  REAL :: c
+  INTEGER :: r
+  INTEGER , INTENT(IN) :: nproma
+  INTEGER :: proma
+
+!$acc parallel
+!$acc loop
+  DO proma = 1 , nproma , 1
+   c = 5.345
+   DO k = 2 , nz , 1
+    t ( k , proma ) = c * k
+    q ( k , proma ) = q ( k - 1 , proma ) + t ( k , proma ) * c
+   END DO
+   q ( nz , proma ) = q ( nz , proma ) * c
+  END DO
+!$acc end parallel
+ END FUNCTION compute_column
+
+ SUBROUTINE compute ( nz , q , nproma )
+  INTEGER , INTENT(IN) :: nz
+  REAL :: t ( 1 : nz , 1 : nproma )
+  REAL , INTENT(INOUT) :: q ( : , : )
+  INTEGER :: result
+  INTEGER , INTENT(IN) :: nproma
+
+  result = compute_column ( nz , q , t , nproma = nproma )
+ END SUBROUTINE compute
+
+END MODULE mo_column
+
