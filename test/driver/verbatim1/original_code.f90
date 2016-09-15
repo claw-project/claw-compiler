b6aff1f61eabad50c95b0c657ff63c272900fb6c
Valentin Clement <clementval@gmail.com>
2016-09-15 15:42:09 +0200

    Add test case for verbatim

---
+++ test/driver/verbatim1/original_code.f90
@@ -0,0 +1,10 @@
+PROGRAM testverbatim
+
+  !$claw verbatim IF (.FALSE.) THEN
+  PRINT*,'These lines'
+  PRINT*,'are not printed'
+  PRINT*,'if the the CLAW compiler has processed'
+  PRINT*,'the file.'
+  !$claw verbatim END IF
+
+END PROGRAM testverbatim
