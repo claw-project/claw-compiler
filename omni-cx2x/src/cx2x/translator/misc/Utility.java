80023f143a0845038ee6f780b996a66633dd26c9
Valentin Clement <clementval@gmail.com>
2016-10-17 09:39:08 +0200

    Format translator.misc package

--- omni-cx2x/src/cx2x/translator/misc/Utility.java
+++ omni-cx2x/src/cx2x/translator/misc/Utility.java
@@ -13,21 +13,23 @@ import java.util.List;
 /**
  * This class contains utilities methods that are useful in the whole project.
  * Mostly to implements java 1.8 features that are not present in java 1.7.
- * 
+ *
  * @author clementval
  */
 public class Utility {
+
   /**
    * Join an array of String elements into a single String.
+   *
    * @param delimiter Delimiter to be placed between each element.
    * @param elements  Array of String elements.
    * @return A joined string of all elements separated by the delimiter.
    */
-  public static String join(String delimiter, String[] elements){
+  public static String join(String delimiter, String[] elements) {
     StringBuilder ret = new StringBuilder();
     boolean firstIteration = true;
-    for(String s : elements){
-      if(!firstIteration){
+    for(String s : elements) {
+      if(!firstIteration) {
         ret.append(delimiter);
       } else {
         firstIteration = false;
@@ -39,15 +41,16 @@ public class Utility {
 
   /**
    * Join a list of String elements into a single String.
+   *
    * @param delimiter Delimiter to be placed between each element.
    * @param elements  List of String elements.
    * @param <T>       Type of element in the list.
    * @return A joined string of all elements separated by the delimiter.
    */
-  public static <T> String join(String delimiter, List<T> elements){
+  public static <T> String join(String delimiter, List<T> elements) {
     StringBuilder ret = new StringBuilder();
     Iterator<T> it = elements.iterator();
-    while(it.hasNext()){
+    while(it.hasNext()) {
       ret.append(it.next().toString());
       if(it.hasNext()) {
         ret.append(delimiter);
@@ -58,9 +61,10 @@ public class Utility {
 
   /**
    * Print debugging information on the standard output if option is active.
+   *
    * @param value Value to be printed.
    */
-  public static void debug(String value){
+  public static void debug(String value) {
     if(XmOption.isDebugOutput()) {
       System.out.println(value);
     }
