83418b10f840f8b30a4318311629f833519fe7eb
Valentin Clement <clementval@gmail.com>
2016-11-22 14:22:09 +0100

    Separate preprocessor compiler config

---
+++ compiler/cray.cmake
@@ -0,0 +1,8 @@
+# For Cray Compiler
+set(FPPFLAGS "-e P")                  # for preprocessing only
+set(FPP_REDIRECT false)               # cannot use redirection > to save file
+set(CLAW_TEST_FFP_FLAGS "-e Z")       # for preprocessing and compilation
+set(TEST_BASE_FLAGS "-h noacc,noomp") # Unactivate OpenACC and OpenMP
+set(OPENACC_FLAGS "-h acc,noomp")     # flags to compile with OpenACC support
+set(OPENMP_FLAGS "-h noacc,omp")      # flags to compile with OpenMP support
+set(OMNI_TARGET "--target=Cray-linux-gnu") # Compilation of OMNI compiler
---
+++ compiler/pgi.cmake
@@ -0,0 +1,9 @@
+# For PGI compiler
+set(FPPFLAGS "-E")                      # for preprocessing only
+set(FPP_REDIRECT true)                  # use redirection > to save file
+set(CLAW_TEST_FFP_FLAGS "-Mpreprocess") # force preprocessing
+set(TEST_BASE_FLAGS "")                 # Base flags for test case compilation
+set(OPENACC_FLAGS "-acc")               # flags to compile with OpenACC support
+                                        # TODO maybe need -ta=<target>
+set(OPENMP_FLAGS "-mp")                 # flags to compile with OpenMP support
+set(OMNI_CONF_OPTION ${OMNI_CONF_OPTION} "CPP=pgcc -E")
