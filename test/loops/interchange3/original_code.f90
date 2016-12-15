7465715fda6a8200d8c87d824d94bfb9b294e8d3
Valentin Clement <clementval@gmail.com>
2016-12-15 17:28:48 +0100

    Add functional test case for interchange directive with target clause

---
+++ test/loops/interchange3/original_code.f90
@@ -0,0 +1,22 @@
+! Simple program to test the loop-interchange directive
+
+PROGRAM LOOP_INTERCHANGE
+  CALL clawloop
+END
+
+SUBROUTINE clawloop
+  INTEGER :: i, j
+  !$claw loop-interchange
+  DO i=1,10
+    DO j=1,2
+      PRINT *, 'Iteration ',i,',',j
+    END DO
+  END DO
+
+  !$claw loop-interchange target(cpu)
+  DO i=1,10
+    DO j=1,2
+      PRINT *, 'Iteration ',i,',',j
+    END DO
+  END DO
+END
