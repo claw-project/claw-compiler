93dedc33131c46619ef2238fbb1d4022a18b8d7a
Valentin Clement <clementval@gmail.com>
2016-12-15 17:22:22 +0100

    Add functional test case for remove directive with target clause

---
+++ test/utilities/remove4/original_code.f90
@@ -0,0 +1,26 @@
+! Simple program to test the remove directive
+
+PROGRAM remove4
+  CALL dummy
+END PROGRAM remove4
+
+SUBROUTINE dummy
+  INTEGER :: k,i,z
+  INTEGER :: kend = 5, iend = 10
+  DO k=1, kend
+    DO i=1, iend
+      ! loop #1 body here
+    END DO
+
+    !$claw remove target(gpu)
+    PRINT*, k
+    PRINT*, k+1
+    !$claw end remove
+
+    !$claw remove target(cpu)
+    z = k + 1
+    z = z**2
+    !$claw end remove
+  END DO
+
+END SUBROUTINE dummy
---
+++ test/utilities/remove4/reference.f90
@@ -0,0 +1,22 @@
+PROGRAM remove4
+
+ CALL dummy ( )
+END PROGRAM remove4
+
+SUBROUTINE dummy ( )
+ INTEGER :: k
+ INTEGER :: i
+ INTEGER :: z
+ INTEGER :: kend = 5
+ INTEGER :: iend = 10
+
+ DO k = 1 , kend , 1
+  DO i = 1 , iend , 1
+  END DO
+!$claw remove target(cpu)
+  z = k + 1
+  z = z ** ( 2 )
+!$claw end remove
+ END DO
+END SUBROUTINE dummy
+
