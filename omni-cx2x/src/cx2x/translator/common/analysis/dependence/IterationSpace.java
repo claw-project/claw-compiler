d3a544f8640cab339225fa0817a7db944df4766a
Valentin Clement <clementval@gmail.com>
2016-12-21 15:39:50 +0100

    Move class to correct package

--- omni-cx2x/src/cx2x/translator/common/analysis/dependence/IterationSpace.java
+++ omni-cx2x/src/cx2x/translator/common/analysis/dependence/IterationSpace.java
@@ -4,6 +4,7 @@
  */
 package cx2x.translator.common.analysis.dependence;
 
+import cx2x.translator.common.analysis.dependence.DependenceAnalysis;
 import cx2x.translator.language.base.ClawLanguage;
 import cx2x.translator.transformation.loop.LoopFusion;
 import cx2x.xcodeml.transformation.DependentTransformationGroup;
