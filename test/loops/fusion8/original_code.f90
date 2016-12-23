08ccd06d1a9e0e25b426d337763fb30b963ed3a9
Valentin Clement <clementval@gmail.com>
2016-12-23 11:47:37 +0100

    Add test case for impossible loop fusion

---
+++ test/loops/fusion8/original_code.f90
@@ -0,0 +1,17 @@
+program test
+  integer :: i, j
+  !$claw loop-fusion
+  do i = 1, 10
+    j = i
+  end do
+
+  j = 20
+
+  !$claw loop-fusion
+  do i = 1, 10
+    if ( j < 15 ) then
+      j = i
+      print*,'HERE'
+    end if
+  end do
+end program test
