adcc63b3e8236e37c0c0cc924974da0a2f2438b6
Valentin Clement <clementval@gmail.com>
2017-02-27 14:18:13 +0100

    Add test sets clean/transform targets
    #104

--- test/base_test.cmake
+++ test/base_test.cmake
@@ -64,6 +64,8 @@ add_custom_target(
 # Add target to the global build/clean target
 add_dependencies(${BUILD_TEST_TARGET} transform-${TEST_NAME})
 add_dependencies(${CLEAN_TEST_TARGET} clean-${TEST_NAME})
+add_dependencies(${BUILD_TEST_TARGET}-${TEST_SET} transform-${TEST_NAME})
+add_dependencies(${CLEAN_TEST_TARGET}-${TEST_SET} clean-${TEST_NAME})
 
 # Define additional compilation flags
 set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ${CLAW_TEST_FFP_FLAGS}")
