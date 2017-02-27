027053e3f885ce78796f3ccb3a132036ae1139ba
Valentin Clement <clementval@gmail.com>
2017-02-27 10:41:15 +0100

    Update reference for OMNI 1.1.1

--- test/claw/abstraction16/reference_cpu.f90
+++ test/claw/abstraction16/reference_cpu.f90
@@ -1,19 +1,21 @@
 MODULE mo_column
  TYPE :: ty_column
-  CONTAINS
-  PROCEDURE :: compute_column
+ CONTAINS
+  PROCEDURE , PASS :: compute_column => compute_column
  END TYPE ty_column
 
 CONTAINS
  SUBROUTINE compute_column ( this , nz , q , t , nproma )
-  CLASS(ty_column) :: this
+  CLASS ( ty_column ) :: this
   INTEGER , INTENT(IN) :: nz
   REAL , INTENT(INOUT) :: t ( : , : )
   REAL , INTENT(INOUT) :: q ( : , : )
   INTEGER :: k
   REAL :: c
+
   INTEGER , INTENT(IN) :: nproma
   INTEGER :: proma
+
 
   c = 5.345
   DO k = 2 , nz , 1
