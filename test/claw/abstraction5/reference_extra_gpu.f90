b350ff992a2be0552e4bcbd7532a81e6cd8c3e0f
Valentin Clement <clementval@gmail.com>
2017-02-27 11:08:12 +0100

    Update missing refs for abstraction5

--- test/claw/abstraction5/reference_extra_gpu.f90
+++ test/claw/abstraction5/reference_extra_gpu.f90
@@ -6,7 +6,9 @@ CONTAINS
   INTEGER , INTENT(IN) :: nz
   REAL , INTENT(INOUT) :: t ( : , : )
   REAL , INTENT(INOUT) :: q ( : , : )
+
   INTEGER , INTENT(IN) :: nproma
+
 
   CALL compute_column ( nz , q , t , nproma = nproma )
  END SUBROUTINE compute
