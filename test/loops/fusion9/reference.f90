5510e52164bb02f39880851b9366e6506d851ea6
Valentin Clement <clementval@gmail.com>
2017-03-30 15:37:39 +0200

    Update reference

--- test/loops/fusion9/reference.f90
+++ test/loops/fusion9/reference.f90
@@ -3,12 +3,13 @@ PROGRAM test
  LOGICAL :: ldo
 
  ldo = .TRUE.
- IF ( ldo ) THEN
 !$claw loop-fusion group(g1) constraint(none)
-  DO i = 1 , 10 , 1
-   PRINT * ,"Loop body #1"
-   PRINT * ,"Loop body #2"
-  END DO
+ DO i = 1 , 10 , 1
+  PRINT * ,"Loop body #1"
+  PRINT * ,"Loop body #2"
+ END DO
+ IF ( ldo ) THEN
+  PRINT * ,"I did"
  END IF
 END PROGRAM test
 
