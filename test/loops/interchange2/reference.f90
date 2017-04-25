c3bdcd68c9f4c05b6b3572ee2c8eee0d46c478b6
Valentin Clement <clementval@gmail.com>
2017-04-25 12:51:12 +0200

    Update reference for interchange2 test case

--- test/loops/interchange2/reference.f90
+++ test/loops/interchange2/reference.f90
@@ -1,5 +1,6 @@
 PROGRAM loop_interchange
 
+
  CALL clawloop ( )
 END PROGRAM loop_interchange
 
@@ -13,6 +14,8 @@ SUBROUTINE clawloop ( )
  INTEGER :: x
  INTEGER :: y
  INTEGER :: z
+
+
 
  DO k = 1 , 2 , 1
   DO i = 1 , 4 , 1
