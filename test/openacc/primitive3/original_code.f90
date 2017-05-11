5b11434a4a412f150adac89278e31f4b07ad8c44
Valentin Clement <clementval@gmail.com>
2017-05-11 14:34:57 +0200

    Add test case for pre-continued pragma

---
+++ test/openacc/primitive3/original_code.f90
@@ -0,0 +1,24 @@
+
+
+PROGRAM test_primitive
+
+  INTEGER :: i
+
+  !$claw acc data present(q,&
+  !$claw acc& p,&
+  !$claw acc& h)&
+  !$claw acc& create(pt)
+
+  !$claw acc parallel
+  DO i = 1, 10
+    PRINT*,i
+  END DO
+  !$claw acc end parallel
+
+  !$claw omp do
+  DO i = 1, 10
+    PRINT*,i
+  END DO
+  !$claw omp end do
+
+END PROGRAM test_primitive
