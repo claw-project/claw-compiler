6612c713163403cd9160b6cc820573c529a8eb47
Valentin Clement <clementval@gmail.com>
2017-05-29 22:47:04 +0200

    Add test case

---
+++ test/loops/ifextract2/original_code.f90
@@ -0,0 +1,18 @@
+! Simple program to test the if-extract directive
+
+program ifextract2
+  integer :: i
+  logical :: test
+
+  test = .TRUE.
+
+  !$claw if-extract
+  DO i = 1, 10
+    IF (test) THEN
+      PRINT *, 'Then body:', i
+    ELSE
+      PRINT *, 'Else body:', i
+    END IF
+  END DO
+
+end program ifextract2
