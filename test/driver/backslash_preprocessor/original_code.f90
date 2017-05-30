c2cd4e26a2ed2f5c0ff2f8595ce77ec6cc3c39e1
Valentin Clement <clementval@gmail.com>
2017-05-30 22:52:59 +0200

    Add backslash

--- test/driver/backslash_preprocessor/original_code.f90
+++ test/driver/backslash_preprocessor/original_code.f90
@@ -25,8 +25,8 @@ test
   INTEGER, PARAMETER :: p2 =  37
   INTEGER, PARAMETER :: p3 =  12
   INTEGER, PARAMETER :: p4 = 307
-  INTEGER, PARAMETER :: sp = SELECTED_REAL_KIND(p1,p2) !< comment \\\
-  INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(p3,p4) !< comment \\
+  INTEGER, PARAMETER :: sp = SELECTED_REAL_KIND(p1,p2) !< comment \
+  INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(p3,p4) !< comment \
   INTEGER, PARAMETER :: wp = dp                        !< selected working precision
   INTEGER, PARAMETER :: vp = wp
   INTEGER, PARAMETER :: vp2 = wp
@@ -35,8 +35,8 @@ test
   INTEGER, PARAMETER :: pi4 =  9
   INTEGER, PARAMETER :: pi8 = 14
   !
-  INTEGER, PARAMETER :: i2 = SELECTED_INT_KIND(pi2)   ! some int \
-  INTEGER, PARAMETER :: i4 = SELECTED_INT_KIND(pi4)   ! some other int \
+  INTEGER, PARAMETER :: i2 = SELECTED_INT_KIND(pi2)   ! some int \\
+  INTEGER, PARAMETER :: i4 = SELECTED_INT_KIND(pi4)   ! some other int \\\\\
   INTEGER, PARAMETER :: i8 = SELECTED_INT_KIND(pi8)   !< dummy comment here
 
 END MODULE mod1
