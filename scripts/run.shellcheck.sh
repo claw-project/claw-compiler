a8e530b2a6544f8aa53c72ec5da9f33404816f5d
Valentin Clement <clementval@gmail.com>
2017-05-31 14:56:53 +0200

    Integrate shellcheck in CI

---
+++ scripts/run.shellcheck.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd driver/bin || exit 1
+shellcheck ../etc/claw_f.conf
+shellcheck ../libexec/claw_f_lib.sh.in
+shellcheck clawfc.in ../libexec/claw_f_lib.sh.in ../etc/claw_f.conf.in
+cd - || exit 1
