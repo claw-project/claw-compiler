2240af7fbf3576956cef3ca7537da10f2cec98cf
Valentin Clement <clementval@gmail.com>
2017-06-12 15:21:23 +0200

    Exit if shellcheck fails

--- scripts/run.shellcheck.sh
+++ scripts/run.shellcheck.sh
@@ -10,10 +10,25 @@
 #
 
 shellcheck -V
-shellcheck offline.sh
-shellcheck pack_release.sh
+if ! shellcheck offline.sh
+then
+  exit 1
+fi
+if ! shellcheck pack_release.sh
+then
+  exit 1
+fi
 cd ../driver/bin || exit 1
-shellcheck ../etc/claw_f.conf
-shellcheck ../libexec/claw_f_lib.sh.in
-shellcheck clawfc.in ../libexec/claw_f_lib.sh.in ../etc/claw_f.conf.in
+if ! shellcheck ../etc/claw_f.conf
+then
+  exit 1
+fi
+if ! shellcheck ../libexec/claw_f_lib.sh.in
+then
+  exit 1
+fi
+if ! shellcheck clawfc.in ../libexec/claw_f_lib.sh.in ../etc/claw_f.conf.in
+then
+  exit 1
+fi
 cd - || exit 1
