33d849755c226b42dfc686313c21fedca3f00a74
Valentin Clement <clementval@gmail.com>
2017-10-17 10:20:49 +0100

    Add test case for #287

---
+++ test/claw/abstraction/abstraction31/mo_column.f90
@@ -0,0 +1,49 @@
+MODULE mo_column
+  IMPLICIT NONE
+  TYPE t1
+    REAL, ALLOCATABLE :: y(:,:)
+  END TYPE t1
+CONTAINS
+
+  SUBROUTINE compute(nz, q, t, z)
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN)   :: nz   ! Size of the array field
+    REAL, INTENT(INOUT)   :: t(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: q(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: z    ! Field declared as scalar
+
+    !$claw parallelize forward
+    CALL compute_column(nz, q, t, z)
+
+  END SUBROUTINE compute
+
+
+  ! Compute only one column
+  SUBROUTINE compute_column(nz, q, t, z)
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN)   :: nz   ! Size of the array field
+    REAL, INTENT(INOUT)   :: t(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: q(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: z    ! Field declared as scalar
+    INTEGER :: k                  ! Loop index
+    REAL :: c                     ! Coefficient
+
+    ! CLAW definition
+
+    ! Define one dimension that will be added to the variables defined in the
+    ! data clause.
+    ! Apply the parallelization transformation on this subroutine.
+
+    !$claw define dimension proma(1:nproma) &
+    !$claw parallelize data(t,q,z) over(proma,:)
+
+    c = 5.345
+    DO k = 2, nz
+      t(k) = c * k
+      q(k) = q(k - 1)  + t(k) * c
+    END DO
+    q(nz) = q(nz) * c
+  END SUBROUTINE compute_column
+END MODULE mo_column
