1b73d17089269805da7b854a84b19466bc1cb5f4
Valentin Clement <clementval@gmail.com>
2018-02-12 13:33:10 +0100

    Update omni_generate_xmod

--- cmake/omni_compiler.cmake
+++ cmake/omni_compiler.cmake
@@ -3,34 +3,39 @@ if(__omni_compiler)
 endif()
 set(__omni_compiler YES)
 
-function(omni_generate_xmod _target _input)
-  if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_input})
-    message(FATAL "Input file ${CMAKE_CURRENT_SOURCE_DIR}/${_input} does not exists !")
+function(omni_generate_xmod)
+  set(oneValueArgs TARGET SOURCE)
+  cmake_parse_arguments(omni_generate_xmod "" "${oneValueArgs}" "" ${ARGN} )
+
+  if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${omni_generate_xmod_SOURCE})
+    message(FATAL "Input file ${CMAKE_CURRENT_SOURCE_DIR}/${omni_generate_xmod_SOURCE} does not exists !")
   endif()
+
+  add_custom_target(${omni_generate_xmod_TARGET} ALL)
 
   set(FPP_ARG_LIST ${FPPFLAGS})
   separate_arguments(FPP_ARG_LIST)
 
   if("${CMAKE_Fortran_COMPILER_ID}" MATCHES "Cray")
-    string(REGEX REPLACE "\\.[^.]*$" "" CRAY_PP_OUTPUT ${_input})
+    string(REGEX REPLACE "\\.[^.]*$" "" CRAY_PP_OUTPUT ${omni_generate_xmod_SOURCE})
     add_custom_command(
-      TARGET ${_target}
+      TARGET ${omni_generate_xmod_TARGET}
       COMMAND ${CMAKE_Fortran_COMPILER} ${FPP_ARG_LIST}
-        ${CMAKE_CURRENT_SOURCE_DIR}/${_input}
+        ${CMAKE_CURRENT_SOURCE_DIR}/${omni_generate_xmod_SOURCE}
       COMMAND
         ${OMNI_F_FRONT} -M${CMAKE_CURRENT_BINARY_DIR}
         ${CMAKE_CURRENT_BINARY_DIR}/"${CRAY_PP_OUTPUT}.i"
-      DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_input}
-      COMMENT "Generating .xmod file for ${_input}"
+      DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${omni_generate_xmod_SOURCE}
+      COMMENT "Generating .xmod file for ${omni_generate_xmod_SOURCE}"
     )
   else()
     add_custom_command(
-      TARGET ${_target}
+      TARGET ${omni_generate_xmod_TARGET}
       COMMAND ${CMAKE_Fortran_COMPILER} ${FPP_ARG_LIST}
-        ${CMAKE_CURRENT_SOURCE_DIR}/${_input} |
+        ${CMAKE_CURRENT_SOURCE_DIR}/${omni_generate_xmod_SOURCE} |
         ${OMNI_F_FRONT} > /dev/null
-      DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_input}
-      COMMENT "Generating .xmod file for ${_input}"
+      DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${omni_generate_xmod_SOURCE}
+      COMMENT "Generating .xmod file for ${omni_generate_xmod_SOURCE}"
     )
   endif()
 endfunction()
