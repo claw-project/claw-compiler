c1cf050e36e0457d183484bb8339004003022019
Valentin Clement <clementval@gmail.com>
2018-02-28 14:45:48 +0100

    Add test data

---
+++ test/claw/abstraction/abstraction35/main.f90
@@ -0,0 +1,28 @@
+!
+! This file is released under terms of BSD license
+! See LICENSE file for more information
+!
+
+PROGRAM test_column_conditional
+  USE mo_column, ONLY: compute_column
+  REAL, DIMENSION(5,10) :: q, t, z  ! Fields as declared in the whole model
+  INTEGER :: nproma, nz             ! Size of array fields
+  INTEGER :: p                      ! Loop index
+
+  nproma = 5
+  nz = 10
+
+  DO p = 1, nproma
+    q(p, 1:nz) = 6.0
+    z(p, 1:nz) = 5.0
+    t(p, 1:6) = 2.0
+    t(p, 6:nz) = 0.0
+  END DO
+
+  !$claw parallelize forward
+  DO p = 1, nproma
+    CALL compute_column(nz, q(p,:), t(p,:), z(p,:))
+  END DO
+
+  PRINT*, (q(1, i), i=1,10)
+END PROGRAM test_column_conditional
---
+++ test/claw/abstraction/abstraction35/mo_column.f90
@@ -0,0 +1,38 @@
+!
+! This file is released under terms of BSD license
+! See LICENSE file for more information
+!
+
+MODULE mo_column
+  IMPLICIT NONE
+CONTAINS
+  ! Compute only one column
+  SUBROUTINE compute_column(nz, q, t, z)
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN)   :: nz   ! Size of the array field
+    REAL, INTENT(INOUT)   :: t(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: q(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: z(:) ! Field declared as one column only
+    INTEGER :: k                  ! Loop index
+
+    ! CLAW definition
+
+    ! Define one dimension that will be added to the variables defined in the
+    ! data clause.
+    ! Apply the parallelization transformation on this subroutine.
+
+    !$claw define dimension proma(1:nproma) &
+    !$claw parallelize
+
+    DO k = 1, nz
+      IF (t(k) > 0.) THEN
+        IF(k < 10) THEN
+          q(k) = q(k) / t(k)
+        END IF
+      ELSE
+        q(k) = q(k) * z(k)
+      END IF
+    END DO
+  END SUBROUTINE compute_column
+END MODULE mo_column
