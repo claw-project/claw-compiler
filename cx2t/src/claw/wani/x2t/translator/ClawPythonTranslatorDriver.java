c65abbaa9a2ffb041afb68a7eb6902d892e14577
Valentin Clement <clementval@gmail.com>
2018-03-12 15:45:36 +0100

    Format + cleanup imports

--- cx2t/src/claw/wani/x2t/translator/ClawPythonTranslatorDriver.java
+++ cx2t/src/claw/wani/x2t/translator/ClawPythonTranslatorDriver.java
@@ -4,17 +4,6 @@
  */
 package claw.wani.x2t.translator;
 
-import claw.tatsu.common.Context;
-import claw.tatsu.common.Message;
-import claw.tatsu.primitive.Pragma;
-import claw.tatsu.xcodeml.xnode.common.Xcode;
-import claw.tatsu.xcodeml.xnode.common.XcodeProgram;
-import claw.wani.ClawConstant;
-import claw.wani.language.ClawPragma;
-import claw.wani.transformation.ClawTransformation;
-import claw.wani.x2t.configuration.Configuration;
-import claw.wani.x2t.translator.ClawTranslatorDriver;
-
 /**
  * ClawPythonTranslatorDriver is the class that wraps the use of a Python
  * script in order to perform the translation.
@@ -28,19 +17,19 @@ public class ClawPythonTranslatorDriver extends ClawTranslatorDriver {
   /**
    * ClawPythonTranslatorDriver constructor.
    *
-   * @param transScript The python recipe input file path.
+   * @param transScript       The python recipe input file path.
    * @param xcodemlInputFile  The XcodeML input file path.
    * @param xcodemlOutputFile The XcodeML output file path.
    */
   public ClawPythonTranslatorDriver(String transScript,
-		                    String xcodemlInputFile,
-			            String xcodemlOutputFile)
+                                    String xcodemlInputFile,
+                                    String xcodemlOutputFile)
       throws Exception
   {
     _transformScript = transScript;
     _xcodemlInputFile = xcodemlInputFile;
     _xcodemlOutputFile = xcodemlOutputFile;
-    
+
     System.out.println("Creating factory object");
 
   }
