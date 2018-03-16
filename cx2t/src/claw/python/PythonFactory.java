6e884cc148c94530468824e264cbdf219f492f36
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-03-16 10:07:54 +0000

    #4 tidy-up placehold python-interface code

--- cx2t/src/claw/python/PythonFactory.java
+++ cx2t/src/claw/python/PythonFactory.java
@@ -1,23 +1,23 @@
+/*
+ * This file is released under terms of BSD license
+ * See LICENSE file for more information
+ */
 package claw.python;
 
 /* These modules are provided by Jython */
 import org.python.core.PyObject;
 
+/**
+ * Placeholder class where we will use Jython to create a
+ * PythonInterpreter object.
+ *
+ * @author A. R. Porter, STFC
+ */
 public class PythonFactory {
 
     private PyObject transformClass;
 
-    /**
-     * Create a new PythonInterpreter object, then use it to
-     * execute some python code. In this case, we want to
-     * import the python module that we will coerce.
-     *
-     * Once the module is imported than we obtain a reference to
-     * it and assign the reference to a Java variable
-     */
-
     public PythonFactory()
     {
     }
-
 }
