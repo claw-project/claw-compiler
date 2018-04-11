2f666c5ca6e2163464e885bf08b722f4aa5ec4a2
Valentin Clement <clementval@gmail.com>
2018-04-11 15:28:30 +0200

    Update intel config for fpp

--- compiler/intel.cmake
+++ compiler/intel.cmake
@@ -8,10 +8,10 @@ endif()
 set(__claw_base_compiler YES)
 
 # Std option for gfortran
-set(FPPFLAGS "-E")            # for preprocessing only
+set(FPPFLAGS "-fpp -E")       # for preprocessing only
 set(FPP_REDIRECT true)        # use redirection > to save file
 set(CLAW_TEST_FFP_FLAGS "-E") # force preprocessing
 set(TEST_BASE_FLAGS "")       # Base flags for test case compilation
 set(OPENACC_FLAGS "")         # flags to compile with OpenACC support
 set(OPENMP_FLAGS "-openmp")   # flags to compile with OpenMP support
-set(COMPILER_MACRO "-D__INTEL_COMPILER -D__ICC")    # predefined macro by compiler
+set(COMPILER_MACRO "-D__ICC") # predefined macro by compiler
