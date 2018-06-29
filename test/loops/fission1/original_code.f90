891539fc16d75e6a17b511d85b3f985c4a4a5c71
Michael Lange <Michael.Lange@ecmwf.int>
2018-06-29 15:45:42 +0100

    Fission: Add simple test setup for initial development

---
+++ test/loops/fission1/original_code.f90
@@ -0,0 +1,27 @@
+!
+! This file is released under terms of BSD license
+! See LICENSE file for more information
+!
+! Simple program to test the loop-fusion directive
+!
+
+PROGRAM LOOP_FUSION
+  CALL clawloop
+END
+
+SUBROUTINE clawloop
+  INTEGER :: i
+
+  DO i=1,10
+    PRINT *, 'First loop body:',i
+
+    !$claw loop-fission
+
+    PRINT *, 'Second loop body:',i
+
+    !$claw loop-fission
+
+    PRINT *, 'Third loop body:',i
+  END DO
+
+END
---
+++ test/loops/fission1/reference.f90
@@ -0,0 +1,20 @@
+PROGRAM loop_fusion
+
+ CALL clawloop ( )
+END PROGRAM loop_fusion
+
+SUBROUTINE clawloop ( )
+
+ INTEGER :: i
+
+ DO i = 1 , 10 , 1
+  PRINT * ,"First loop body:" , i
+ END DO
+ DO i = 1 , 10 , 1
+  PRINT * ,"Second loop body:" , i
+ END DO
+ DO i = 1 , 10 , 1
+  PRINT * ,"Third loop body:" , i
+ END DO
+END SUBROUTINE clawloop
+
