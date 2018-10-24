339eaa5ae2dad16196767afb66b0913acaec1ad2
Valentin Clement <clementval@gmail.com>
2018-10-24 10:46:20 +0200

    Add test case for call-site collapse in sca foward

---
+++ test/claw/sca/sca43/mo_column.f90
@@ -0,0 +1,45 @@
+!
+! This file is released under terms of BSD license
+! See LICENSE file for more information
+!
+
+MODULE mo_column
+  IMPLICIT NONE
+
+  TYPE ty_column
+
+  CONTAINS
+    PROCEDURE :: compute_column
+  END TYPE ty_column
+
+CONTAINS
+
+  ! Compute only one column
+  SUBROUTINE compute_column(this, nz, q, t)
+    IMPLICIT NONE
+
+    CLASS(ty_column)      :: this
+    INTEGER, INTENT(IN)   :: nz   ! Size of the array field
+    REAL, INTENT(INOUT)   :: t(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: q(:) ! Field declared as one column only
+    INTEGER :: k                  ! Loop index
+    REAL :: c                     ! Coefficient
+
+    ! CLAW definition
+
+    ! Define one dimension that will be added to the variables defined in the
+    ! data clause.
+    ! Apply the parallelization transformation on this subroutine.
+
+    !$claw define dimension proma(1:nproma) &
+    !$claw parallelize
+
+    c = 5.345
+    DO k = 2, nz
+      t(k) = c * k
+      q(k) = q(k - 1)  + t(k) * c
+    END DO
+    q(nz) = q(nz) * c
+  END SUBROUTINE compute_column
+
+END MODULE mo_column
---
+++ test/claw/sca/sca43/mo_column_extra.f90
@@ -0,0 +1,41 @@
+!
+! This file is released under terms of BSD license
+! See LICENSE file for more information
+!
+
+MODULE mo_column_extra
+  USE mo_column, ONLY: ty_column, compute_column
+  IMPLICIT NONE
+
+CONTAINS
+
+  SUBROUTINE compute_one(nz, q, t)
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN)   :: nz   ! Size of the array field
+    REAL, INTENT(INOUT)   :: t(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: q(:) ! Field declared as one column only
+    TYPE(ty_column)       :: column
+
+    !$claw parallelize forward
+    CALL column%compute_column(nz, q, t)
+
+  END SUBROUTINE compute_one
+
+  SUBROUTINE compute_two(nz, q, t)
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN)   :: nz   ! Size of the array field
+    REAL, INTENT(INOUT)   :: t(:) ! Field declared as one column only
+    REAL, INTENT(INOUT)   :: q(:) ! Field declared as one column only
+    TYPE(ty_column)       :: column
+
+    !$claw define dimension proma(1:nproma) &
+    !$claw parallelize
+
+    
+    q = q + sum(t)
+
+  END SUBROUTINE compute_two
+
+END MODULE mo_column_extra
