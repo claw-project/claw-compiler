654eac7a1bfc3c299dcc5edd818e889f40544816
Valentin Clement <clementval@gmail.com>
2018-10-31 13:28:49 +0100

    Update reference

--- test/openacc/primitive2/reference.f90
+++ test/openacc/primitive2/reference.f90
@@ -3,7 +3,7 @@ PROGRAM test_primitive
  INTEGER :: i
  INTEGER :: nd
 
- nd = acc_get_num_devices ( 4 )
+ nd = acc_get_num_devices ( acc_device_nvidia )
 !$acc parallel
  DO i = 1 , 10 , 1
   PRINT * , i
