addc5fb7cc7a1544600b4b800c8bbdb8a1755163
Valentin Clement <clementval@gmail.com>
2018-11-14 07:37:51 +0100

    Fix option

--- cx2t/src/claw/ClawX2T.java
+++ cx2t/src/claw/ClawX2T.java
@@ -127,7 +127,7 @@ public class ClawX2T {
     options.addOption("x", true,
         "override configuration option. Higher priority over base " +
             "configuration and user configuration.");
-    options.addOption("ap", "--add-paren", false,
+    options.addOption("ap", "add-paren", false,
         "Force backend to add parenthesis in binary mathematical binary " +
             "operation.");
     return options;
