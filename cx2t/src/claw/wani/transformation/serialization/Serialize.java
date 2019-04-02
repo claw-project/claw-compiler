4379a6d435fd2f9055759c784902ca071f231b30
Valentin Clement <clementval@gmail.com>
2019-04-02 15:34:50 +0200

    Find function call

--- cx2t/src/claw/wani/transformation/serialization/Serialize.java
+++ cx2t/src/claw/wani/transformation/serialization/Serialize.java
@@ -6,7 +6,9 @@ package claw.wani.transformation.serialization;
 
 import claw.shenron.transformation.Transformation;
 import claw.shenron.translator.Translator;
+import claw.tatsu.xcodeml.xnode.common.Xcode;
 import claw.tatsu.xcodeml.xnode.common.XcodeProgram;
+import claw.tatsu.xcodeml.xnode.common.Xnode;
 import claw.wani.language.ClawPragma;
 import claw.wani.transformation.ClawTransformation;
 
@@ -14,6 +16,8 @@ import claw.wani.transformation.ClawTransformation;
  * @author phmarti, havogt, clementval
  */
 public class Serialize extends ClawTransformation {
+
+  private Xnode _fctCall;
 
   /**
    * Constructs a new LoopFusion triggered from a specific pragma.
@@ -27,6 +31,22 @@ public class Serialize extends ClawTransformation {
 
   @Override
   public boolean analyze(XcodeProgram xcodeml, Translator translator) {
+    Xnode next = _claw.getPragma().nextSibling();
+    if(next == null) {
+      xcodeml.addError("Directive is not followed by a valid statement.",
+          _claw.getPragma());
+      return false;
+    }
+    if(Xnode.isOfCode(next, Xcode.EXPR_STATEMENT)
+        || Xnode.isOfCode(next, Xcode.F_ASSIGN_STATEMENT))
+    {
+      _fctCall = next.matchSeq(Xcode.FUNCTION_CALL);
+      if(_fctCall == null) {
+        xcodeml.addError("Function call not found",
+            _claw.getPragma());
+        return false;
+      }
+    }
     return true;
   }
 
