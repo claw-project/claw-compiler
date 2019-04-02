024ceeb65d7833e0f54212471ce62624cc82c67a
Philippe Marti <philippe.marti@env.ethz.ch>
2019-04-02 15:14:34 +0200

    Add serialize directive test

---
+++ test/serialization/serialize1/original_code.f90
@@ -0,0 +1,31 @@
+!
+! This file is released under terms of BSD license
+! See LICENSE file for more information
+!
+! Simple program to test the serialize directive
+!
+PROGRAM SERIALIZE
+  INTEGER :: i = 4
+  INTEGER :: j = 2
+  INTEGER :: k = 6
+  REAL, DIMENSION(10) :: a, b, c
+  REAL, DIMENSION(10,20) :: aa, bb, cc
+  REAL, DIMENSION(10,20,30) :: aaa, bbb, ccc
+
+  !$claw serialize banana1
+  CALL clawserialize(i, a, aa, aaa,j ,b, bb, bbb,k ,c, cc, ccc)
+
+  CONTAINS
+
+    SUBROUTINE clawserialize(i, a, aa, aaa, j, b, bb, bbb, k, c, cc, ccc)
+      INTEGER, INTENT(IN)     :: i
+      REAL, INTENT(IN)        :: a(:), aa(:,:), aaa(:,:,:)
+      INTEGER, INTENT(OUT)    :: j
+      REAL, INTENT(OUT)       :: b(:), bb(:,:), bbb(:,:,:)
+      INTEGER, INTENT(INOUT)  :: k
+      REAL, INTENT(INOUT)     :: c(:), cc(:,:), ccc(:,:,:)
+
+
+    END
+
+END
