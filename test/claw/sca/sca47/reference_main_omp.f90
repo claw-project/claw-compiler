a33c065ba72c7343fffe2bf27e4d22b7977e8e22
clementval <clementval@gmail.com>
2019-12-05 15:50:26 -0500

    Update main refs

--- test/claw/sca/sca47/reference_main_omp.f90
+++ test/claw/sca/sca47/reference_main_omp.f90
@@ -1,2 +1,9 @@
+!
+! This file is released under terms of BSD license
+! See LICENSE file for more information
+!
+! Test the CLAW abstraction model with one additional dimension.
+!
+
 program dummy
 end program
