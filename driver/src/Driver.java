0da3050126c301abb75aac8dcc6718e24b7a091e
Mikhail Zhigun <Mikhail.Zhigun@meteoswiss.ch>
2020-09-07 19:18:04 +0200

    Added manifest to driver executable

--- driver/src/Driver.java
+++ driver/src/Driver.java
@@ -1,9 +1,12 @@
 package clawfc;
 
+import claw.ClawX2T;
+
 public class Driver
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         System.out.println("This is clawfc");
+        claw.ClawX2T.main(new String[]{"--help"});
     }
 };
