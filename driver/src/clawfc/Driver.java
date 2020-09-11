39a3915db8be7ba9a9ea772863eda5f034997d3f
Mikhail Zhigun <Mikhail.Zhigun@meteoswiss.ch>
2020-09-11 15:38:52 +0200

    Added java clawfc cmdline opts spec

--- driver/src/clawfc/Driver.java
+++ driver/src/clawfc/Driver.java
@@ -3,6 +3,7 @@ package clawfc;
 import net.sourceforge.argparse4j.*;
 import net.sourceforge.argparse4j.inf.*;
 import net.sourceforge.argparse4j.helper.*;
+import net.sourceforge.argparse4j.impl.Arguments;
 
 public class Driver
 {
@@ -13,65 +14,62 @@ public class Driver
             ArgumentParser parser = ArgumentParsers.newFor("clawfc").build().description("The CLAW Compiler is a " +
                     "source-to-source translator working on the XcodeML intermediate representation");
             ArgumentGroup cOpts = parser.addArgumentGroup("Compiler options");
+            cOpts.addArgument("-o", "--output-file").help(
+            		"Output file for the transformed FORTRAN code. If not given, code is printed to stdout.");
+            cOpts.addArgument("-I", "--include-dir").nargs("*").help(
+            		"Add the directory dir to the search path for .mod and .xmod files.");
+            cOpts.addArgument("-J", "--output-mod-dir").help("Output directory for compiled .mod and .xmod files.");
+            cOpts.addArgument("-t", "--target").help("Type of accelerator directive language for code generation");
+            cOpts.addArgument("--list-targets", "--target-list").action(Arguments.storeTrue()).help(
+            		"List the available type of accelerator directive language supported");
+            cOpts.addArgument("-d", "--directive").help(
+            		"Specify the type of accelerator directive language for code generation");
+            cOpts.addArgument("--list-directives", "--directive-list").action(Arguments.storeTrue()).help(
+            		"List the available type of accelerator directive language supported");
+            cOpts.addArgument("--config").help("Specify a different configuration for the translator");
+            cOpts.addArgument("--show-config").action(Arguments.storeTrue()).help("List the current configuration information."
+            		+ " If used with --config, list the information from the specific configuration");
+            cOpts.addArgument("-m", "--model-config").help("Specific model configuration for SCA");
+            cOpts.addArgument("-c", "--keep-comment").action(Arguments.storeTrue()).help("Keep comments in the transformed file");
+            cOpts.addArgument("-v", "--verbose").action(Arguments.storeTrue()).help("Print processing status");
+            cOpts.addArgument("--version").action(Arguments.storeTrue()).help("Print version");
+            cOpts.addArgument("--show-env").action(Arguments.storeTrue()).help("Show environment variables");
+            cOpts.addArgument("--no-dep").action(Arguments.storeTrue()).help("Don't generate .mod or .xmod file for dependencies");
+            cOpts.addArgument("--no-module-cache").action(Arguments.storeTrue()).help("Deactivate module cache in the front-end");
+            cOpts.addArgument("-f", "--force").action(Arguments.storeTrue()).help(
+            		"Force the translation of files without directives");
+            cOpts.addArgument("--force-pure").action(Arguments.storeTrue()).help(
+            		"Force compiler to exit when transformation applied to PURE subroutine/function");
+            cOpts.addArgument("--add-paren").action(Arguments.storeTrue()).help(
+            		"Add parenthesis to binary operation in generated code");
+            cOpts.addArgument("-r", "--report").action(Arguments.storeTrue()).help("Generate the transformation report");
+            cOpts.addArgument("--debug").action(Arguments.storeTrue()).help("Display transformation debug information");
+            cOpts.addArgument("--debug-omni").action(Arguments.storeTrue()).help(
+            		"Save intermediate files in __omni_tmp__ and display driver information");
+            cOpts.addArgument("--debug-ffront").action(Arguments.storeTrue()).help("Drive OMNI Fortran front-end in debug mode");
+            cOpts.addArgument("--stop-pp").action(Arguments.storeTrue()).help("Save intermediate files and stop after preprocess");
+            cOpts.addArgument("--stop-dependencies").action(Arguments.storeTrue()).help(
+            		"Save intermediate files and stop after dependencies resolution");
+            cOpts.addArgument("--stop-frontend").action(Arguments.storeTrue()).help(
+            		"Save intermediate files and stop after frontend");
+            cOpts.addArgument("--stop-translator").action(Arguments.storeTrue()).help(
+            		"Save intermediate files and stop after translator");
+            cOpts.addArgument("-x").nargs("*").help("Override a configuration key:value pair from the command line. Higher "
+            		+ "priority over base configuration and user configuration");
+            ArgumentGroup dcOpts = parser.addArgumentGroup("Decompiler options");
+            dcOpts.addArgument("-w").type(Integer.class).help("Set the number of columns for the output FORTRAN file (default: 80)");
+            dcOpts.addArgument("-l").action(Arguments.storeTrue()).help(
+            		"Add preprocessor line directives in the output FORTRAN file");
+            ArgumentGroup pOpts = parser.addArgumentGroup("Process options");
+            pOpts.addArgument("--Wp").nargs("*").help("Add preprocessor option");
+            pOpts.addArgument("--Wf").nargs("*").help("Add frontend option");
+            pOpts.addArgument("--Wx").nargs("*").help("Add Xcode translator option");
             parser.parseArgs(args);
         }
         catch(HelpScreenException hse)
         {
             System.exit(0);
         }
-        /*parser = argparse.ArgumentParser(description='CLAW')
-        c_opts = parser.add_argument_group('Compiler options')
-        c_opts.add_argument('-o', '--output-file', type=str,
-            help='Output file for the transformed FORTRAN code. If not given, code is printed to stdout.')
-        c_opts.add_argument('-I', '--include-dir', type=str, nargs='*',
-            help='Add the directory dir to the search path for .mod and .xmod files.')
-        c_opts.add_argument('-J', '--output-mod-dir', type=str, help='Output directory for ccompiled .mod and .xmod files.')
-        c_opts.add_argument('-O', '--output-dir', type=str, help='Output directory for transformed files.')
-        c_opts.add_argument('-t', '--target', type=str, help='Type of accelerator directive language for code generation')
-        c_opts.add_argument('--list-targets', '--target-list', action='store_true',
-            help='List the available type of accelerator directive language supported')
-        c_opts.add_argument('-d', '--directive', type=str,
-            help='Specify the type of accelerator directive language for code generation')
-        c_opts.add_argument('--list-directives', '--directive-list', action='store_true',
-            help='List the available type of accelerator directive language supported')
-        c_opts.add_argument('--config', type=str, help='specify a different configuration for the translator')
-        c_opts.add_argument('--show-config', action='store_true',
-            help='List the current configuration information. If used with --config, list the information from \
-                  the specific configuration')
-        c_opts.add_argument('-m', '--model-config', type=str, help='Specific model configuration for SCA')
-        c_opts.add_argument('-c', '--keep-comment', action='store_true', help='Keep comments in the transformed file')
-        c_opts.add_argument('-v', '--verbose', action='store_true', help='Print processing status')
-        c_opts.add_argument('--version', action='store_true', help='Print version')
-        c_opts.add_argument('--show-env', action='store_true', help='Show environment variables')
-        c_opts.add_argument('--no-dep', action='store_true', help="Don't generate .mod or .xmod file for dependencies")
-        c_opts.add_argument('--no-module-cache', action='store_true', help='Deactivate module cache in the front-end')
-        c_opts.add_argument('-f', '--force', action='store_true', help='Force the translation of files without directives')
-        c_opts.add_argument('--force-pure', action='store_true',
-                            help='Force compiler to exit when transformation applied to PURE subroutine/function')
-        c_opts.add_argument('--add-paren', action='store_true',
-                            help='Add parenthesis to binary operation in generated code')
-        c_opts.add_argument('-r', '--report', action='store_true', help='Generate the transformation report')
-        c_opts.add_argument('--debug', action='store_true', help='Display transformation debug information')
-        c_opts.add_argument('--debug-omni', action='store_true',
-                            help='Save intermediate files in __omni_tmp__ and display driver information')
-        c_opts.add_argument('--debug-ffront', action='store_true', help='Drive OMNI Fortran front-end in debug mode')
-        c_opts.add_argument('--stop-pp', action='store_true', help='Save intermediate files and stop after preprocess')
-        c_opts.add_argument('--stop-dependencies', action='store_true',
-                            help='Save intermediate files and stop after dependencies resolution')
-        c_opts.add_argument('--stop-frontend', action='store_true',
-                            help='Save intermediate files and stop after frontend')
-        c_opts.add_argument('--stop-translator', action='store_true',
-                            help='Save intermediate files and stop after translator')
-        c_opts.add_argument('-x', type=str, nargs='*', help='Override a configuration key:value pair from the command '
-                            'line. Higher priority over base configuration and user configuration')
-        dc_opts = parser.add_argument_group('Decompiler options')
-        dc_opts.add_argument('-w', type=int, help='Set the number of columns for the output FORTRAN file (default: 80)')
-        dc_opts.add_argument('-l', action='store_true', help='Add preprocessor line directives in the output FORTRAN file')
-        p_opts = parser.add_argument_group('Process options')
-        p_opts.add_argument('--Wp', type=str, nargs='*', help='Add preprocessor option')
-        p_opts.add_argument('--Wf', type=str, nargs='*', help='Add frontend option')
-        p_opts.add_argument('--Wx', type=str, nargs='*', help='Add Xcode translator option')
-        p_args = parser.parse_args()*/
         System.exit(0);
     }
 };
