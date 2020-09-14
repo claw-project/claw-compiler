545bf5288294d8eb9d9c3033460d4ee5618ed313
Mikhail Zhigun <Mikhail.Zhigun@meteoswiss.ch>
2020-09-14 19:14:55 +0200

    Added java wrapper and verification for claw driver cfg

--- driver/src/clawfc/Configuration.java
+++ driver/src/clawfc/Configuration.java
@@ -4,32 +4,100 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.logging.Logger;
+import java.lang.System;
+import java.lang.StringBuilder;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 public class Configuration
 {
     public static final String PROPERTIES_FILENAME = "/config/clawfc.properties";
     private final static Logger LOGGER = Logger.getLogger(Configuration.class.getName());
 
-    public Configuration() throws IOException
+    public String name()
     {
-	cfgProperties = loadProperties();
+        return cfgProperties.getProperty("claw.name");
     }
 
-    Properties loadProperties() throws IOException
+    public String version()
     {
-	Properties props = new Properties();
-	InputStream inStream;
-	try
-	{
-	    inStream = Configuration.class.getResourceAsStream(PROPERTIES_FILENAME);
-	    props.load(inStream);
-	} catch (Exception e)
-	{
-	    LOGGER.severe(String.format("Failed to load properties config file \"%s\"", PROPERTIES_FILENAME));
-	    throw e;
-	}
-	return props;
+        return cfgProperties.getProperty("claw.version");
+    }
+
+    public String defaultFortranCompilerType()
+    {
+        return cfgProperties.getProperty("claw.fc.default");
+    }
+
+    public Path installRoot()
+    {
+        return Paths.get(cfgProperties.getProperty("claw.home"));
+    }
+
+    public String omniVersion()
+    {
+        return cfgProperties.getProperty("omni.version.string");
+    }
+
+    public String omniVersionTag()
+    {
+        return cfgProperties.getProperty("omni.version.tag");
+    }
+
+    public Path omniInstallRoot()
+    {
+        if (OMNI_HOME_ENV != null)
+        {
+            return Paths.get(OMNI_HOME_ENV);
+        } else
+        {
+            return Paths.get(cfgProperties.getProperty("omni.home"));
+        }
+    }
+
+    public Path omniFrontEnd()
+    {
+        return omniInstallRoot().resolve(cfgProperties.getProperty("omni.fortran.frontend")).normalize();
+    }
+
+    public Configuration() throws Exception
+    {
+        cfgProperties = loadProperties();
+        String name = this.name();
+    }
+
+    Properties loadProperties() throws Exception
+    {
+        Properties props = new Properties();
+        InputStream inStream;
+        try
+        {
+            inStream = Configuration.class.getResourceAsStream(PROPERTIES_FILENAME);
+            props.load(inStream);
+        } catch (Exception e)
+        {
+            LOGGER.severe(String.format("Failed to load properties config file \"%s\"", PROPERTIES_FILENAME));
+            throw e;
+        }
+        return props;
+    }
+
+    public String toString()
+    {
+        StringBuilder s = new StringBuilder();
+        s.append(String.format("claw.name : %s\n", name()));
+        s.append(String.format("claw.version : %s\n", version()));
+        s.append(String.format("claw.defaultFortranCompilerType : %s\n", defaultFortranCompilerType()));
+        s.append(String.format("claw.installRoot : %s\n", installRoot()));
+        s.append(String.format("omni.version.string : %s\n", omniVersion()));
+        s.append(String.format("omni.version.tag : %s\n", omniVersionTag()));
+        s.append(String.format("omni.home : %s [%s]\n", omniInstallRoot().toString(),
+                cfgProperties.getProperty("omni.home")));
+        s.append(String.format("omni.fortran.frontend : %s [%s]\n", omniFrontEnd().toString(),
+                cfgProperties.getProperty("omni.fortran.frontend")));
+        return s.toString();
     }
 
     Properties cfgProperties;
+    static final String OMNI_HOME_ENV = System.getenv("OMNI_HOME");
 };
