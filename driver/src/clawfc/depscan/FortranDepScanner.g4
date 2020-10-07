53c3230c40c23f720013e18d4a4aa3aaa307f789
Mikhail Zhigun <Mikhail.Zhigun@meteoswiss.ch>
2020-10-07 17:23:44 +0200

    Added ANTLR grammar for Fortran dependencies scan; Added unit tests coverage of the grammar

---
+++ driver/src/clawfc/depscan/FortranDepScanner.g4
@@ -0,0 +1,70 @@
+/**
+ * ANTLR 4 Grammar file for the Fortran dependencies scanner.
+ *
+ * @author Mikhail Zhigun
+ * @copyright 2020, MeteoSwiss
+ */
+grammar FortranDepScanner;
+
+proc_stmt : PROC_STMT EOF;
+
+PROC_STMT : SEP? (MODULE_OPEN_STMT | MODULE_CLOSE_STMT | PROGRAM_OPEN_STMT | PROGRAM_CLOSE_STMT | USE_STMT);
+
+MODULE_OPEN_STMT : MODULE SEP IDENTIFIER SEP?;
+MODULE_CLOSE_STMT : END SEP MODULE SEP IDENTIFIER SEP?;
+
+PROGRAM_OPEN_STMT : PROGRAM SEP IDENTIFIER SEP?;
+PROGRAM_CLOSE_STMT : END SEP PROGRAM SEP IDENTIFIER SEP?;
+
+USE_STMT : USE SEP IDENTIFIER SEP? (',' SEP? (RENAME_LIST_STMT | ONLY_LIST_STMT))?;
+
+RENAME_LIST_STMT : RENAME_STMT (',' SEP? RENAME_STMT)*;
+RENAME_STMT : IDENTIFIER SEP? '=>' SEP? IDENTIFIER SEP?;
+
+ONLY_LIST_STMT : ONLY SEP? ':' SEP? ONLY_STMT (',' SEP? ONLY_STMT)*;
+ONLY_STMT : IDENTIFIER SEP? ('=>' SEP? IDENTIFIER SEP?)?;
+
+fragment USE : U S E;
+fragment MODULE : M O D U L E;
+fragment ONLY : O N L Y;
+fragment PROGRAM : P R O G R A M;
+fragment END : E N D;
+
+IDENTIFIER : LETTER (LETTER | DIGIT | '_')*;
+
+fragment LETTER : [a-zA-Z];
+fragment DIGIT : [0-9];
+
+fragment SEP : WS+;
+fragment WS : [ \t\r];
+fragment EOL : '\n';
+
+fragment A : [aA];
+fragment B : [bB];
+fragment C : [cC];
+fragment D : [dD];
+fragment E : [eE];
+fragment F : [fF];
+fragment G : [gG];
+fragment H : [hH];
+fragment I : [iI];
+fragment J : [jJ];
+fragment K : [kK];
+fragment L : [lL];
+fragment M : [mM];
+fragment N : [nN];
+fragment O : [oO];
+fragment P : [pP];
+fragment Q : [qQ];
+fragment R : [rR];
+fragment S : [sS];
+fragment T : [tT];
+fragment U : [uU];
+fragment V : [vV];
+fragment W : [wW];
+fragment X : [xX];
+fragment Y : [yY];
+fragment Z : [zZ];
+
+ERROR_CHAR : .;
+
---
+++ driver/unittests/src/clawfc/ut/TestDepScanParser.java
@@ -0,0 +1,220 @@
+/*
+ * @author Mikhail Zhigun
+ * @copyright Copyright 2020, MeteoSwiss
+ */
+package clawfc.ut;
+
+import org.junit.Test;
+import static junit.framework.TestCase.*;
+import org.antlr.v4.runtime.*;
+import clawfc.depscan.parser.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import junit.framework.TestCase;
+
+public class TestDepScanParser 
+    extends TestCase 
+{
+  private FortranDepScannerParser parser;
+  private FortranDepScannerLexer lexer;
+  
+  @Override
+  protected void setUp() throws Exception 
+  {
+      lexer = new FortranDepScannerLexer(toCharStream(""));
+      lexer.removeErrorListener(ConsoleErrorListener.INSTANCE);
+      parser = new FortranDepScannerParser(toTokenStream(""));
+      parser.removeErrorListener(ConsoleErrorListener.INSTANCE);
+  }
+  
+  private static CharStream toCharStream(String str) throws IOException
+  {
+      InputStream inStrm = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));  
+      CharStream chrStrm = CharStreams.fromStream(inStrm, StandardCharsets.UTF_8);
+      return chrStrm;
+  }
+  
+  private CommonTokenStream toTokenStream(String str) throws IOException
+  {
+      CharStream chrStrm = toCharStream(str);
+      lexer.reset();
+      lexer.setInputStream(chrStrm);
+      CommonTokenStream tokStrm = new CommonTokenStream(lexer);
+      return tokStrm;
+  }
+  
+  private void acceptString(String str) throws IOException
+  {
+      parser.reset();
+      parser.setInputStream(toTokenStream(str));
+      parser.proc_stmt();
+      assertTrue(String.format("Failed to accept string \"%s\"", str), parser.isMatchedEOF() && parser.getNumberOfSyntaxErrors() == 0);
+  }
+  
+  private void rejectString(String str) throws IOException
+  {
+      parser.reset();
+      parser.setInputStream(toTokenStream(str));
+      parser.proc_stmt();
+      assertTrue(String.format("Failed to reject string \"%s\"", str), !parser.isMatchedEOF() && parser.getNumberOfSyntaxErrors() > 0);
+  }
+  
+  private String flipChrCase(String str, int idx)
+  {
+      char c = str.charAt(idx);
+      if(c >= 'a' && c <= 'z')
+      { Character.toUpperCase(c); }
+      else 
+      { Character.toLowerCase(c); }
+      str = str.substring(0, idx) + c + str.substring(idx + 1);
+      return str;      
+  }
+
+  public void testIdentifier() throws Exception
+  {
+      for(char l = 'a'; l < 'z'; ++l)
+      {
+          acceptString("module " + l); 
+          acceptString("module " + l + '_');
+          for(char l2 = '0'; l2 < '9'; ++l2)
+          {
+              acceptString("module " + l + l2);
+          }
+      }
+      for(char l = 'A'; l < 'Z'; ++l)
+      {
+          acceptString("module " + l); 
+          acceptString("module " + l + '_');
+          for(char l2 = '0'; l2 < '9'; ++l2)
+          {
+              acceptString("module " + l + l2);
+          }          
+      }
+      rejectString("module _");
+      for(char l2 = '0'; l2 < '9'; ++l2)
+      {
+          rejectString("module " + l2);
+      }    
+  }
+
+  public void testModuleOpenStatement() throws Exception
+  {
+      acceptString("module x");
+      acceptString(" module x");
+      acceptString("\tmodule x");
+      acceptString("\rmodule x");
+      acceptString("module x ");
+      acceptString("module x\r");
+      acceptString("module x\t");
+      for(int i = 0, n = "module".length(); i < n; ++i)
+      {
+          String str = flipChrCase("module", i) + " x";
+          acceptString(str);
+      }
+      rejectString("module");
+      rejectString("modulex");  
+      rejectString("module x !");  
+      rejectString("module x y");  
+  }
+
+  public void testModuleCloseStatement() throws Exception
+  {
+      acceptString("end module x");
+      acceptString(" end module x");
+      acceptString("\rend module x");
+      acceptString("\tend module x");
+      acceptString("\tend\r\rmodule  x ");
+      for(int i = 0, n = "end".length(); i < n; ++i)
+      {
+          String str = flipChrCase("end", i) + " module x";
+          acceptString(str);
+      }
+      for(int i = 0, n = "module".length(); i < n; ++i)
+      {
+          String str = "end " + flipChrCase("module", i) + " x";
+          acceptString(str);
+      }
+      rejectString("\tend\r\rmodule  x z");
+  }
+
+  public void testProgramOpenStatement() throws Exception
+  {
+      acceptString("program x");
+      acceptString(" program x");
+      acceptString("\tprogram x");
+      acceptString("\rprogram x");
+      acceptString("program x ");
+      acceptString("program x\r");
+      acceptString("program x\t");
+      for(int i = 0, n = "program".length(); i < n; ++i)
+      {
+          String str = flipChrCase("program", i) + " x";
+          acceptString(str);
+      }
+      rejectString("program");
+      rejectString("programx");  
+      rejectString("program x !");  
+      rejectString("program x y");  
+  }
+
+  public void testProgramCloseStatement() throws Exception
+  {
+      acceptString("end Program x");
+      acceptString(" end Program x");
+      acceptString("\rend Program x");
+      acceptString("\tend Program x");
+      acceptString("\tend\r\rProgram  xy_23");
+      for(int i = 0, n = "end".length(); i < n; ++i)
+      {
+          String str = flipChrCase("end", i) + " Program x";
+          acceptString(str);
+      }
+      for(int i = 0, n = "Program".length(); i < n; ++i)
+      {
+          String str = "end " + flipChrCase("Program", i) + " x";
+          acceptString(str);
+      }
+      rejectString("\tend\r\rprogram x z");
+  }
+
+  public void testUseStatement() throws Exception
+  {
+      acceptString("use x");
+      for(int i = 0, n = "use".length(); i < n; ++i)
+      {
+          String str = flipChrCase("use", i) + " x";
+          acceptString(str);
+      }
+      acceptString(" use x");
+      acceptString(" use x ");
+      acceptString("use x,y1=>z1");
+      acceptString("use x,  y1=>z1");
+      acceptString("use x,  y1  =>z1");
+      acceptString("use x,  y1  =>  z1");
+      acceptString("use x,  y1  =>  z1  ");
+      acceptString("use x,  y1  =>  z1, y2 => z2 ");
+      acceptString("use x,  y1  =>  z1, y2 => z2, y3 => z3 ");
+      acceptString("use x,  ONLY: y1");
+      for(int i = 0, n = "only".length(); i < n; ++i)
+      {
+          String str = "use x,  " + flipChrCase("only", i) + ":x";
+          acceptString(str);
+      }
+      acceptString("use x ,  ONLY : y1");
+      acceptString("use x,  ONLY : y1");
+      acceptString("use x,  ONLY: y1,y2");
+      acceptString("use x,  ONLY: y1,y2,y3");
+      acceptString("use x,  ONLY:y1=>z1");
+      acceptString("use x,  ONLY: y1 =>z1");
+      acceptString("use x,  ONLY: y1  =>z1");
+      acceptString("use x,  ONLY: y1  =>  z1");
+      acceptString("use x,  ONLY: y1  =>  z1  ");
+      acceptString("use x,  ONLY: y1  =>  z1, y2 => z2, y3 => z3 ");
+      rejectString("usex");
+      rejectString("use x,");
+      rejectString("use x y ");
+      rejectString("use x, y,");
+      rejectString("use x, ONLY ");
+      rejectString("use x, ONLY: x y");
+  }
+}
