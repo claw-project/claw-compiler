51ff0ad116b62d8fcf13adbf30788e64348b8078
Mikhail Zhigun <Mikhail.Zhigun@meteoswiss.ch>
2020-12-27 21:25:01 +0100

    Added xast generation test with src file

--- driver/tests/src/clawfc/tests/XastGenerationTest.java
+++ driver/tests/src/clawfc/tests/XastGenerationTest.java
@@ -25,8 +25,8 @@ public class XastGenerationTest extends clawfc.tests.utils.DriverTestCase
 
     public void testInput() throws Exception
     {
-        final Path INPUT_FILEPATH = RES_DIR.resolve("xast_generation/input/1.f90");
-        final Path REF_MOD_DIR = RES_DIR.resolve("xast_generation/reference");
+        final Path INPUT_FILEPATH = RES_DIR.resolve("xast_generation/without_src_files/input/1.f90");
+        final Path REF_MOD_DIR = RES_DIR.resolve("xast_generation/without_src_files/reference");
         final Path OUT_XAST_DIR = TMP_DIR.resolve("xast");
         List<String> xastNames = Arrays.asList("mod11.xast", "mod12.xast", "mod13.xast", "p1.xast");
         String[] args = new String[] { "--stop-xast-gen", "--disable-mp", "-XO", OUT_XAST_DIR.toString(),
@@ -43,10 +43,31 @@ public class XastGenerationTest extends clawfc.tests.utils.DriverTestCase
         assertFalse(fileExists(OUT_XAST_DIR.resolve("mod_no_claw.xast")));
     }
 
+    public void testInputWithSourceFile() throws Exception
+    {
+        final Path INPUT_FILEPATH = RES_DIR.resolve("xast_generation/with_src_files/input/1.f90");
+        final Path REF_MOD_DIR = RES_DIR.resolve("xast_generation/with_src_files/reference");
+        final Path OUT_XAST_DIR = TMP_DIR.resolve("xast");
+        List<String> xastNames = Arrays.asList("mod11.xast", "mod12.xast", "mod13.xast", "p1.xast");
+        String[] args = new String[] { "--stop-xast-gen", "--disable-mp", "--skip-pp", "-XO", OUT_XAST_DIR.toString(),
+                INPUT_FILEPATH.toString() };
+        run(args);
+        for (String modName : xastNames)
+        {
+            Path resFilePath = OUT_XAST_DIR.resolve(modName);
+            Path refFilePath = REF_MOD_DIR.resolve(modName);
+            String resStr = removeType(removeTime(collectIntoString(resFilePath)));
+            String refStr = removeType(removeTime(collectIntoString(refFilePath))).replace("{filename}",
+                    INPUT_FILEPATH.toString());
+            assertEquals(refStr, resStr);
+        }
+        assertFalse(fileExists(OUT_XAST_DIR.resolve("mod_no_claw.xast")));
+    }
+
     public void testForceInput() throws Exception
     {
-        final Path INPUT_FILEPATH = RES_DIR.resolve("xast_generation/input/1.f90");
-        final Path REF_MOD_DIR = RES_DIR.resolve("xast_generation/reference");
+        final Path INPUT_FILEPATH = RES_DIR.resolve("xast_generation/without_src_files/input/1.f90");
+        final Path REF_MOD_DIR = RES_DIR.resolve("xast_generation/without_src_files/reference");
         final Path OUT_XAST_DIR = TMP_DIR.resolve("xast");
         List<String> xastNames = Arrays.asList("mod11.xast", "mod12.xast", "mod13.xast", "mod_no_claw.xast", "p1.xast");
         String[] args = new String[] { "--stop-xast-gen", "--force", "--disable-mp", "-XO", OUT_XAST_DIR.toString(),
@@ -64,8 +85,8 @@ public class XastGenerationTest extends clawfc.tests.utils.DriverTestCase
 
     public void testMultiprocessing() throws Exception
     {
-        final Path INPUT_FILEPATH = RES_DIR.resolve("xast_generation/input/1.f90");
-        final Path REF_MOD_DIR = RES_DIR.resolve("xast_generation/reference");
+        final Path INPUT_FILEPATH = RES_DIR.resolve("xast_generation/without_src_files/input/1.f90");
+        final Path REF_MOD_DIR = RES_DIR.resolve("xast_generation/without_src_files/reference");
         final Path OUT_XAST_DIR = TMP_DIR.resolve("xast");
         List<String> xastNames = Arrays.asList("mod11.xast", "mod12.xast", "mod13.xast", "mod_no_claw.xast", "p1.xast");
         String[] args = new String[] { "--stop-xast-gen", "--force", "-XO", OUT_XAST_DIR.toString(),
