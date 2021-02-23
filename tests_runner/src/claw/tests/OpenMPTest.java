3854bf65b3775d86acbedbebed41600d9f1526f5
Mikhail Zhigun <Mikhail.Zhigun@meteoswiss.ch>
2021-02-24 00:21:30 +0100

    Fixed CLAW OpenMP test

---
+++ tests_runner/src/claw/tests/OpenMPTest.java
@@ -0,0 +1,40 @@
+/*
+ * @author Mikhail Zhigun
+ * @copyright Copyright 2021, MeteoSwiss
+ */
+package claw.tests;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+
+import claw.utils.BasicTestCase;
+
+public class OpenMPTest extends BasicTestCase
+{
+    InputParams createParams(String name)
+    {
+        final String relpath = "openmp/" + name;
+        final Path resDir = RES_DIR.resolve(relpath);
+        final Path workingDir = WORKING_DIR.resolve(relpath);
+        InputParams p = new InputParams(name, resDir, workingDir);
+        p.setInputDirName("");
+        p.setRefDirName("");
+        p.setOutputDirName("");
+        p.setCompare(false);
+        p.setDebugClawfc(false);
+        return p;
+    }
+
+    void run(String name) throws Exception
+    {
+        InputParams inParams = createParams(name);
+        run(inParams);
+    }
+
+    public void test_primitive1() throws Exception
+    {
+        InputParams inParams = createParams("primitive1");
+        inParams.setClawFlags(Arrays.asList("--target=gpu", "--directive=openmp"));
+        run(inParams);
+    }
+}
