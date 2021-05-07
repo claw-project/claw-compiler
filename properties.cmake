d30775164cbd257cadbbc6eae3911518f1d04f91
Mikhail Zhigun <miz.github@gmail.com>
2021-05-07 21:25:54 +0200

    Updated omni

--- properties.cmake
+++ properties.cmake
@@ -81,7 +81,7 @@ set(CLAW_TESTS_REPORT "${TEST_REPORTS_DIR}/claw.txt")
 #
 # OMNI Compiler variables
 #
-set(OMNI_GIT_COMMIT_HASH "f0c3efcfc1e6ea36a0ff729e5731e943bb523c22" CACHE STRING
+set(OMNI_GIT_COMMIT_HASH "6c2fd4e080767b2e29dc27bad8647f0984892845" CACHE STRING
     "OMNI compiler tools GIT commit hash")
 set(OMNI_GIT_REPOSITORY "https://github.com/claw-project/xcodeml-tools.git" CACHE STRING
     "OMNI compiler tools GIT repository URL")
