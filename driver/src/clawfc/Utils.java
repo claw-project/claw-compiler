79e910ba87750357cf00fd5df53cd7071c3d74c9
Valentin Clement (バレンタイン クレメン) <clementval@gmail.com>
2021-06-10 10:21:39 -0400

    Get temp directory from the property
    Remove hardcoded temporary directory and use java property to get system temporary directory instead. This works on multiple platform and do not assumed any pre-existing directory.

--- driver/src/clawfc/Utils.java
+++ driver/src/clawfc/Utils.java
@@ -56,7 +56,7 @@ public class Utils
     public static final byte ASCII_SPACE_VALUE = (byte) ' ';
     public static final String[] FORTRAN_FILE_EXTENSIONS = new String[] { "f", "F", "f90", "F90", "f95", "F95", "f03",
             "F03" };
-    public static final String DEFAULT_TOP_TEMP_DIR = "/dev/shm";
+    public static final String DEFAULT_TOP_TEMP_DIR = System.getProperty("java.io.tmpdir");
     public static final Path STARTUP_DIR = Paths.get(System.getProperty("user.dir"));
     public static final Logger log = Logger.getLogger("CLAW");
 
